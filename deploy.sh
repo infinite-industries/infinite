59a65ed0c296508e00279bd64f7d2a84703633bc
chriswininger <cwininger@airspringsoftware.com>
2018-04-22 19:20:49 -0400

    updating deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,15 @@
 #!/usr/bin/env bash
 
+npm run production-build
+
 USER='ubuntu'
 
 SERVER=''
 if [[ "production" = $1 ]]; then
-  ROOT='/home/ubuntu/app/new_mvp'
-  SERVER='infinite.industries'
-elif [[ "staging" = $1 ]]; then
   ROOT='/home/ubuntu/front_end_infinite'
-  # SERVER='staging.infinite.industries'
-  SERVER='staging5074.cloudapp.net'
-else
-  echo Please specify environment to deploy to.
-  echo Usage: ./deploy.sh environment
-  echo Example: ./deploy.sh staging
-  exit
-fi
+  SERVER='infinite.industries'
 
-npm run production-build
-
-ssh $USER@$SERVER << EOF
+  ssh $USER@$SERVER bash --login -i << EOF
   cd $ROOT
   echo 'Updating sources'
   git reset --hard HEAD
@@ -30,10 +20,39 @@ ssh $USER@$SERVER << EOF
   echo 'Restarting'
   #npm run server-build
   #echo 'Building frontend js'
-  forever stop mvp
-  rm /home/$USER/.forever/mvp.log
-  forever start --uid mvp server.js
+  forever stop infinite
+  rm /home/$USER/.forever/infinite.log
+  forever start --uid infinite server.js
   echo 'Done!'
 EOF
+elif [[ "staging" = $1 ]]; then
+  ROOT='/home/ubuntu/front_end_infinite'
+  SERVER='staging.infinite.industries'
+
+  ssh $USER@$SERVER bash --login -i  << EOF
+
+  cd $ROOT
+  echo 'Updating sources'
+  git reset --hard HEAD
+  git checkout development
+  git pull
+  echo 'Installing npm packages'
+  npm install --production
+  echo 'Restarting'
+  #npm run server-build
+  #echo 'Building frontend js'
+  forever stop infinite
+  rm /home/$USER/.forever/infinite.log
+  forever start --uid infinite server.js
+  echo 'Done!'
+EOF
+else
+  echo Please specify environment to deploy to.
+  echo Usage: ./deploy.sh environment
+  echo Example: ./deploy.sh staging
+  exit
+fi
+
+npm run production-build
 
 scp public/js/dist.js $USER@$SERVER:$ROOT/public/js
