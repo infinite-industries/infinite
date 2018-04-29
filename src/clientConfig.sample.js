40ac27b3954a17fefa6f19aff58420e35115c00f
dima <shifting.planes@gmail.com>
2018-04-29 18:59:18 -0400

    cleaned up clientConfig

--- src/clientConfig.sample.js
+++ src/clientConfig.sample.js
@@ -1,5 +1,5 @@
 // ____________Auth0_Settings__________________
-export const CLIENT_ID = 'PYKhof4U0jKE3v4h8xKSgihHz9atBE5O'
-export const CLIENT_DOMAIN = '1nfinite.auth0.com1nfinite.auth0.com'
-export const REDIRECT = 'http://localhost:7779/callback'
-export const AUDIENCE = 'https://1nfinite.auth0.com/api/v2/'
+export const CLIENT_ID = ''
+export const CLIENT_DOMAIN = ''
+export const REDIRECT = ''
+export const AUDIENCE = ''
