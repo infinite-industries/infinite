ffbb399ba5b99d990f92ba679fc7ccbf82aab3d1
dima <shifting.planes@gmail.com>
2018-11-11 17:46:59 -0500

    fixed UTC bug for ics generation and are currently rolling our own ics files

--- routes/calendar.js
+++ routes/calendar.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const bodyParser = require('body-parser')
-const ics = require('ics')
-const moment = require('moment')
+//const ics = require('ics')
+const uuidv4 = require('uuid/v4')
+const moment = require('moment-timezone')
 
 router.use(bodyParser.urlencoded({
     extended: true
@@ -10,37 +11,38 @@ router.use(bodyParser.urlencoded({
 
 router.get('/', function(req, res){
   // generate an ICS file for iCal and Outlook
-  // https://www.npmjs.com/package/ics
-  const calendar_entry = ''
-  const title = req.query.title
+
   const file_name = req.query.title + ".ics"
-  const location = req.query.location
-  const description = req.query.description       // TODO sanitize
-  const time_start =  moment(req.query.time_start).toArray().slice(0, 5)
-  const time_end = moment(req.query.time_end).toArray().slice(0, 5)
 
-  // bump month by one since moment represents months 0-11 and ics 1-12
-  time_start[1] = time_start[1]+1
-  time_end[1] = time_end[1]+1
+  let cal_content =
+`BEGIN:VCALENDAR
+VERSION:2.0
+CALSCALE:GREGORIAN
+PRODID:InfiniteIndustries/ics
+METHOD:PUBLISH
+X-PUBLISHED-TTL:PT1H
+BEGIN:VEVENT
+UID:${uuidv4()}
+SUMMARY:${req.query.title}
+DTSTAMP:${moment().utc().format('YYYYMMDDTHHmmss')}Z
+DTSTART:${moment.tz(req.query.time_start, "America/New_York").utc().format('YYYYMMDDTHHmmss')}Z
+DTEND:${moment.tz(req.query.time_end, "America/New_York").utc().format('YYYYMMDDTHHmmss')}Z
+DESCRIPTION:${req.query.description}
+LOCATION:${req.query.location}
+END:VEVENT
+END:VCALENDAR`
 
-  ics.createEvent({
-    title: title,
-    description: description,
-    location: location,
-    start: time_start,
-    end: time_end,
-  }, (error, value) => {
-    if (error) {
-      console.log(error)
-      res.json({"status":"error", "reason":"Unable to generate a calendar file"})
-    } else {
-      res.setHeader('Content-disposition', 'attachment; filename="'+ file_name + '"')
-      res.setHeader('Content-type', 'text/plain')
-      res.charset = 'UTF-8'
-      res.write(value)
-      res.end()
-    }
-  })
+
+  console.log(req.query.time_start +"\n ------- \n")
+  console.log("req:"+req.query.time_start+" utc:"+moment(req.query.time_start).utc().format('YYYYMMDDTHHmmss'))
+  console.log(req.query.time_start +"\n ------- \n")
+  console.log(cal_content)
+
+  res.setHeader('Content-disposition', 'attachment; filename="'+ file_name + '"')
+  res.setHeader('Content-type', 'text/calendar')
+  res.charset = 'UTF-8'
+  res.write(cal_content)
+  res.end()
 
 
 })
