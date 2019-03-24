acb21d818826b8d73b0c6d850a0ded436a2c0551
dima <shifting.planes@gmail.com>
2019-03-24 14:29:44 -0400

    updated deploy for back end

--- api-server/deploy.sh
+++ api-server/deploy.sh
@@ -1,43 +1,48 @@
 #!/usr/bin/env bash
 
-ROOT='/home/ubuntu/infinite'
+ROOT='/home/ubuntu'
 USER='ubuntu'
 
 SERVER=''
 if [[ "production" = $1 ]]; then
   SERVER='api.infinite.industries'
 
-  ssh $USER@$SERVER bash --login -i << EOF
-    cd $ROOT
-    echo 'Updating sources'
-    git reset --hard HEAD
-    git checkout master
-    git pull origin master
-    echo 'Installing npm packages'
-    npm install
-    echo 'Restarting'
-    forever stop infinite
-    rm /home/$USER/.forever/infinite.log
-    forever start --uid infinite index.js
-    echo 'Done!'
+  # ssh $USER@$SERVER bash --login -i << EOF
+  #   cd $ROOT
+  #   echo 'Updating sources'
+  #   git reset --hard HEAD
+  #   git checkout master
+  #   git pull origin master
+  #   echo 'Installing npm packages'
+  #   npm install
+  #   echo 'Restarting'
+  #   forever stop infinite
+  #   rm /home/$USER/.forever/infinite.log
+  #   forever start --uid infinite index.js
+    echo 'Prod build not implemented yet!'
 EOF
 
 elif [[ "staging" = $1 ]]; then
   SERVER='staging-api.infinite.industries'  #not used yet
 
-
   ssh $USER@$SERVER bash --login -i << EOF
-    cd $ROOT
+
+    cd $ROOT/temp-infinite/infinite
     echo 'Updating sources'
     git reset --hard HEAD
-    git checkout master
-    git pull origin master
+    git checkout development
+    git pull
+
+    cd ./api-server
+    cp * -r $ROOT/infinite/.
+    cd $ROOT/infinite
+
     echo 'Installing npm packages'
-    npm install
-    echo 'Restarting'
+    npm install --production
     forever stop infinite
-    rm /home/$USER/.forever/infinite.log
+    rm $ROOT/.forever/infinite.log
     forever start --uid infinite index.js
+
     echo 'Done!'
 EOF
 else
