e03af8abd6919aad9224af00f934a7ffcb8b5716
chriswininger <chris.wininger@gmail.com>
2019-03-24 14:46:42 -0400

    added eslint config

--- web-portal/src/store/store.js
+++ web-portal/src/store/store.js
@@ -7,7 +7,7 @@ import ComponentEventBus from '../helpers/ComponentEventBus'
 import admin from './modules/admin'
 import ui from './modules/ui'
 
-import {getIdToken, getUsername, isAdmin, logout} from '../helpers/Auth'
+import {getIdToken, isAdmin, logout} from '../helpers/Auth'
 
 Vue.use(Vuex)
 
@@ -31,7 +31,7 @@ export const store = new Vuex.Store({
     user_settings:{
       logged_in: false,
       admin_role: false,
-      username: "",
+      username: '',
       associated_venues: []
     },
 
@@ -47,19 +47,19 @@ export const store = new Vuex.Store({
   },
   getters:{
     GetAllDateTimes: state => {
-      return state.calendar_event.date_times;
+      return state.calendar_event.date_times
     },
     GetAllVenues: state => {
-      return state.all_venues;
+      return state.all_venues
     },
     GetLoadingStatus: state => {
-      return state.util.loading;
+      return state.util.loading
     },
     GetMyLists: state => {
-      return state.lists_my;
+      return state.lists_my
     },
     GetListsIFollow: state => {
-      return state.lists_follow;
+      return state.lists_follow
     },
     GetAllLocalEvents: state => {
       return state.all_local_events
@@ -69,7 +69,7 @@ export const store = new Vuex.Store({
       return state.current_list
     },
 
-    GetSettings: state => {
+    GetSettings: () => {
       // TODO
     },
     GetUser: state => {
@@ -83,8 +83,8 @@ export const store = new Vuex.Store({
 
     UPDATE_USER_DATA: (state, user_data) => {
       state.user_settings = {... state.user_settings, ...user_data, username: user_data.name }
-      state.lists_my = user_data.lists_my;
-      state.lists_follow = user_data.lists_follow;
+      state.lists_my = user_data.lists_my
+      state.lists_follow = user_data.lists_follow
 
       state.loaded_from_api = true
     },
@@ -94,53 +94,53 @@ export const store = new Vuex.Store({
     },
 
     CREATE_NEW_EVENT: (state) => {
-        state.calendar_event = {
-          id:"",
-          title: "",
-          date_times: [
+      state.calendar_event = {
+        id:'',
+        title: '',
+        date_times: [
 
-          ],
-          image:"",
-          social_image:"",
-          organizers:"",
-          admission_fee:"none",
-          venue:{
-            name: "",
-            id:"",
-            slug:"",
-            createdAt:"",
-            updatedAt: "",
-            g_map_link:"",
-            address: ""
-          },
-          address: "",
-          brief_description:"",
-          description:"",
-          website_link:"",
-          eventbrite_link:"",
-          fb_event_link:"",
-          ticket_link:"",
-          organizer_contact:"",
-          multi_day: false,
-          additional_dates: []
+        ],
+        image:'',
+        social_image:'',
+        organizers:'',
+        admission_fee:'none',
+        venue:{
+          name: '',
+          id:'',
+          slug:'',
+          createdAt:'',
+          updatedAt: '',
+          g_map_link:'',
+          address: ''
+        },
+        address: '',
+        brief_description:'',
+        description:'',
+        website_link:'',
+        eventbrite_link:'',
+        fb_event_link:'',
+        ticket_link:'',
+        organizer_contact:'',
+        multi_day: false,
+        additional_dates: []
 
-        }
-      },
-      DELETE_TIME_SEGMENT: (state, payload) => {
-        state.calendar_event.date_times.splice(payload.index,1)
-      },
-      ADD_NEW_TIME_SEGMENT: (state) => {
-        state.calendar_event.date_times.push({
-          optional_title:"",
-          start_time:"",
-          end_time:""
-        })
-      },
-      UPDATE_CURRENT_TIME_SEGMENT: (state, payload) => {
-        state.calendar_event.date_times[payload.current_time_segment].optional_title = payload.optional_title
-        state.calendar_event.date_times[payload.current_time_segment].start_time = payload.start_time
-        state.calendar_event.date_times[payload.current_time_segment].end_time = payload.end_time
-      },
+      }
+    },
+    DELETE_TIME_SEGMENT: (state, payload) => {
+      state.calendar_event.date_times.splice(payload.index,1)
+    },
+    ADD_NEW_TIME_SEGMENT: (state) => {
+      state.calendar_event.date_times.push({
+        optional_title:'',
+        start_time:'',
+        end_time:''
+      })
+    },
+    UPDATE_CURRENT_TIME_SEGMENT: (state, payload) => {
+      state.calendar_event.date_times[payload.current_time_segment].optional_title = payload.optional_title
+      state.calendar_event.date_times[payload.current_time_segment].start_time = payload.start_time
+      state.calendar_event.date_times[payload.current_time_segment].end_time = payload.end_time
+    },
 
     UPDATE_LOCALIZED_EVENTS: (state, payload) => {
       state.all_local_events = payload
@@ -154,11 +154,11 @@ export const store = new Vuex.Store({
     PUSH_NEW_EVENT_TO_MY_LIST: (state, payload) => {
 
       let list_index = state.lists_my.findIndex(list => {
-        console.log(list.id + " ---" + payload.list_id);
+        console.log(list.id + ' ---' + payload.list_id)
         return list.id === payload.list_id
       })
 
-      console.log("index now is", list_index);
+      console.log('index now is', list_index)
 
       state.lists_my[list_index].events.push(payload.event_data)
     },
@@ -167,7 +167,7 @@ export const store = new Vuex.Store({
     },
 
     REMOVE_FROM_CURRENT_LIST: (state, payload) =>{
-      console.log("MY CURRENT LIST:", state.current_list)
+      console.log('MY CURRENT LIST:', state.current_list)
       state.current_list.events = state.current_list.events.filter(event => event.id !== payload.id)
     },
 
@@ -175,7 +175,7 @@ export const store = new Vuex.Store({
       state.unverified_events = payload
     },
     CHANGE_STATE_TO_VERIFIED: (state, payload) =>{
-      console.log(state.unverified_events.find(event => event.id === payload.id));
+      console.log(state.unverified_events.find(event => event.id === payload.id))
       state.all_local_events.push(state.unverified_events.find(event => event.id === payload.id))
       state.unverified_events = state.unverified_events.filter(event => event.id !== payload.id)
     },
@@ -184,14 +184,14 @@ export const store = new Vuex.Store({
     //   state.calendar_event = event
     // },
     LOGIN: (state, payload) => {
-      state.user_settings.logged_in = true;
-      state.user_settings.admin_role = payload.admin_role;
+      state.user_settings.logged_in = true
+      state.user_settings.admin_role = payload.admin_role
     },
     LOGOUT: state => {
       state.user_settings = {
         logged_in: false,
         admin_role: false,
-        username: "",
+        username: '',
         associated_venues: []
       }
     }
@@ -199,11 +199,11 @@ export const store = new Vuex.Store({
   },
   actions:{
 
-    Logout: (context, payload) => {
+    Logout: (context) => {
       logout()
       context.commit('LOGOUT')
     },
-    Login: (context, payload) => {
+    Login: (context) => {
       context.commit('LOGIN', { admin_role: isAdmin() })
     },
     CreateNewEvent: (context) =>{
@@ -224,12 +224,11 @@ export const store = new Vuex.Store({
 
 
     CreateNewList: (context, payload) => {
-      const _self = this
       // Hit API to create a list
       Axios.post('/lists/create-new', { list_name:payload.name, description:payload.description })
         .then(function (_response) {
-          console.log(_response.data);
-          if(_response.data.status === "success"){
+          console.log(_response.data)
+          if(_response.data.status === 'success'){
             const empty_list = {
               id: _response.data.id,
               list_name: payload.name,
@@ -240,80 +239,78 @@ export const store = new Vuex.Store({
           }
           else{
             ComponentEventBus.$emit('SHOW_ALERT', {
-              message: "Hrrmm... unable create an new list. Please contact us and we will figure out what went wrong. Code: #00447"
+              message: 'Hrrmm... unable create an new list. Please contact us and we will figure out what went wrong. Code: #00447'
             })
           }
         })
         .catch(function (error) {
-          console.log(error);
+          console.log(error)
           ComponentEventBus.$emit('SHOW_ALERT', {
-            message: "Hrrmm... unable create an new list. Please contact us and we will figure out what went wrong. Code: #00347"
+            message: 'Hrrmm... unable create an new list. Please contact us and we will figure out what went wrong. Code: #00347'
           })
-        });
+        })
     },
-    FollowList: (context, payload) => {
+    FollowList: () => {
       // TODO
     },
-    UnFollowList: (context, payload) => {
+    UnFollowList: () => {
       // TODO
     },
     AddEventToMyList: (context, payload) => {
-      const _self = this
       Axios.post('/events/add',{ event_id:payload.event_data.id, list_id:payload.list_id })
         .then(function (_response) {
-          if(_response.data.status === "success"){
+          if(_response.data.status === 'success'){
             context.commit('PUSH_NEW_EVENT_TO_MY_LIST', {list_id:payload.list_id, event_data:payload.event_data})
             // need to get back the full list
 
             ComponentEventBus.$emit('SHOW_INFO', {
-              message: "Added a new event to your list."
+              message: 'Added a new event to your list.'
             })
           }
           else{
             ComponentEventBus.$emit('SHOW_ALERT', {
-              message: "Hrrmm... unable add this event to your list. Please contact us and we will figure out what went wrong. Code: #33347"
+              message: 'Hrrmm... unable add this event to your list. Please contact us and we will figure out what went wrong. Code: #33347'
             })
           }
         })
         .catch(function (error) {
-          console.log(error);
+          console.log(error)
           ComponentEventBus.$emit('SHOW_ALERT', {
-            message: "Hrrmm... unable add this event to your list. Please contact us and we will figure out what went wrong. Code: #23997"
+            message: 'Hrrmm... unable add this event to your list. Please contact us and we will figure out what went wrong. Code: #23997'
           })
 
-        });
+        })
     },
     RemoveEventFromList: (context, payload) => {
-      const _self = this
       Axios.post('/events/remove',{ event_id:payload.event_id, list_id:payload.list_id })
         .then(function (_response) {
-          if(_response.data.status === "success"){
+          if(_response.data.status === 'success'){
             context.commit('REMOVE_FROM_CURRENT_LIST', _response.data)
 
             ComponentEventBus.$emit('SHOW_INFO', {
-              message: "Event taken off your list."
+              message: 'Event taken off your list.'
             })
           }
           else{
             ComponentEventBus.$emit('SHOW_ALERT', {
-              message: "Hrrmm... unable to remove this event from your list. Please contact us and we will figure out what went wrong. Code: #2347"
+              message: 'Hrrmm... unable to remove this event from your list. Please contact us and we will figure out what went wrong. Code: #2347'
             })
           }
         })
         .catch(function (error) {
-          console.log("error mesg:", error);
+          console.log('error mesg:', error)
           ComponentEventBus.$emit('SHOW_ALERT', {
-            message: "Hrrmm... unable to remove this event from your list. Please contact us and we will figure out what went wrong. Code: #2647"
+            message: 'Hrrmm... unable to remove this event from your list. Please contact us and we will figure out what went wrong. Code: #2647'
           })
 
-        });
+        })
     },
 
     LoadAllUserData: (context) => {
       const showWelcome = () => {
         //Greet users who are not logged in
         ComponentEventBus.$emit('SHOW_INFO', {
-          message: "Welcome! Check out the local cultural awesomeness! Please log in to start saving and sharing event lists. If we accidently missed something cool and cultural in your area, feel free to submit your own event via submissions page."
+          message: 'Welcome! Check out the local cultural awesomeness! Please log in to start saving and sharing event lists. If we accidently missed something cool and cultural in your area, feel free to submit your own event via submissions page.'
         })
       }
 
@@ -327,19 +324,19 @@ export const store = new Vuex.Store({
           showWelcome()
         })
         .catch(function (error) {
-          console.error(`no user data: ${error}`);
+          console.error(`no user data: ${error}`)
           context.commit('UPDATE_USER_DATA', {
             user_settings: {
               logged_in: false,
               admin_role: false,
-              username: "",
+              username: '',
               associated_venues: []
             }
           })
           showWelcome()
-        });
+        })
     },
-    LoadAllLocalEventData: (context, payload) => {
+    LoadAllLocalEventData: (context) => {
       context.commit('SET_LOADING_STATUS', true)
       Axios.get('/events')
         .then(function (_response) {
@@ -347,14 +344,14 @@ export const store = new Vuex.Store({
           context.commit('SET_LOADING_STATUS', false)
         })
         .catch(function (error) {
-          console.log(error);
+          console.log(error)
           ComponentEventBus.$emit('SHOW_ALERT', {
-            message: "Hrrmm... unable to get event data. Please contact us and we will figure out what went wrong."
+            message: 'Hrrmm... unable to get event data. Please contact us and we will figure out what went wrong.'
           })
 
-        });
+        })
     },
-    LoadAllVenueData: (context, payload) => {
+    LoadAllVenueData: (context) => {
       // Axios.get('/events/current/verified')
       Axios.get('/venues')
         .then(function (_response) {
@@ -362,39 +359,37 @@ export const store = new Vuex.Store({
           context.commit('UPDATE_ALL_VENUES', _response.data.venues)
         })
         .catch(function (error) {
-          console.log(error);
+          console.log(error)
           ComponentEventBus.$emit('SHOW_ALERT', {
-            message: "Hrrmm... unable to get event data. Please contact us and we will figure out what went wrong."
+            message: 'Hrrmm... unable to get event data. Please contact us and we will figure out what went wrong.'
           })
 
-        });
+        })
     },
 
     LoadListData:(context, id) => {
       // set current_list that we will be operating on
-      const req_url = "/lists/" + id;
+      const req_url = '/lists/' + id
 
       Axios.get(req_url)
         .then(function (_response) {
           // console.log("data from server: ",response.data.events);
-          if(_response.data.status === "success"){
+          if(_response.data.status === 'success'){
             console.log(_response.data)
             context.commit('POPULATE_CURRENT_LIST', _response.data.eventList)
           }
           else{
             ComponentEventBus.$emit('SHOW_ALERT', {
-              message: "Hrrmm... unable to get list data. Please contact us and we will figure out what went wrong. Code: #11647"
+              message: 'Hrrmm... unable to get list data. Please contact us and we will figure out what went wrong. Code: #11647'
             })
           }
         })
         .catch(function (error) {
-          console.log(error);
+          console.log(error)
           ComponentEventBus.$emit('SHOW_ALERT', {
-            message: "Hrrmm... unable to get list data. Please contact us and we will figure out what went wrong. Code: #11007"
+            message: 'Hrrmm... unable to get list data. Please contact us and we will figure out what went wrong. Code: #11007'
           })
-        });
+        })
     }
-
-
   }
 })
