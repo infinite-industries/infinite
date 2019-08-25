d9622622bdec26698b5ed81875b8bac97984c8fa
Chris Wininger <cwininger@sonatype.com>
2019-08-25 11:31:50 -0400

    Create Slug And Bitly Link

--- api-server/routes/events.js
+++ api-server/routes/events.js
@@ -1,10 +1,16 @@
 // event related API endpoints
+
 const EventController = require('../controllers/events')
 const CurrentEventController = require('../controllers/currentEvents')
 const { getDefaultRouter } = require('./helpers/routeHelpers')
 const { literal } = require('sequelize')
 const JWTAuthenticator = require(__dirname + '/../utils/JWTAuthenticator')
 const DatesToISO = require(__dirname + '/middleware/datesToISO')
+const axios = require('axios')
+const uuidv1 = require('uuid/v1');
+
+const BITLY_URI ='https://api-ssl.bitly.com/v3/shorten'
+const BITLY_TOKEN = process.env.BITLY_TOKEN
 
 const filterContactInfo = (req, data) => {
 	if (req.isInfiniteAdmin) {
@@ -22,7 +28,7 @@ const router = getDefaultRouter("events", "event", EventController, { verified: 
 	// provides special controller methods for getters to merge data from multiple tables
 	allMethod: EventController.allAndMergeWithVenues,
 	byIDMethod: EventController.findByIDAndMergeWithVenues,
-	createMiddleware: [DatesToISO], // anyone can create a new event; Dates will be converted form local to UTC/ISO
+	createMiddleware: [DatesToISO, createOverride], // anyone can create a new event; Dates will be converted form local to UTC/ISO
 	updateMiddleware: [JWTAuthenticator(true)], // requires admin token to update (put)
 	readFilter: filterContactInfo // strip contact info
 });
@@ -91,5 +97,57 @@ router.put(
 		});
 	}
 );
+
+async function createOverride(req, res, next) {
+	if (!req.body.event)
+		return res.status(422).json({ status: 'event parameter is required' })
+
+	try {
+		const id = uuidv1()
+
+		const bitlyLink = await _createBitlyLink(`https://infinite.industries/events/${id}`)
+
+		const postJSON = {
+			...req.body.event,
+			id,
+			bitly_link: bitlyLink,
+			slug: _getSlug(req.body.event.title)
+		}
+
+		CurrentEventController.create(req.app.get('db'), postJSON, async (err) => {
+			if (err) {
+				const msg = 'error creating "event": ' + err
+				console.warn(msg)
+				return res.status(500).json({ status: msg })
+			}
+
+			res.status(200).json({ status: 'success', id: postJSON.id })
+		});
+	} catch (ex) {
+		console.warn('error calling link shortener: ', ex)
+		res.status(500).json({ status: 'error calling link shortener' })
+	}
+}
+
+async function _createBitlyLink(infiniteUrl) {
+	const requestUrl = `${BITLY_URI}?access_token=${BITLY_TOKEN}&longUrl=${encodeURI(infiniteUrl)}`
+
+	const { data } = await axios.get(requestUrl)
+
+
+	if (data.status_code != 200) {
+		throw new Error(`Status ${ data.status_code } returned from link shortener`)
+	} else {
+		return data.data.url
+	}
+}
+
+function _getSlug(title) {
+	if (!title) {
+		return 'missing-title'
+	}
+
+	return title.toLowerCase().replace(/ /g,'-')
+}
 
 module.exports = router;
