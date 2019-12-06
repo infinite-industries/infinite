7a7f51c983ca1b5f316615245bd29e2a39e7172e
Chris Wininger <chris.wininger@gmail.com>
2019-12-06 18:26:02 -0500

    issue-63: remove wild card
    we need to be more explicit about which headers are allowed. Early
    versions of safari don't support wild card.

--- api-server/index.js
+++ api-server/index.js
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET,PUT,HEAD,POST,DELETE,OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'x-access-token, *')
+  res.header('Access-Control-Allow-Headers', 'x-access-token, content-type')
   res.header('Access-Control-Allow-Credentials', 'true')
   next()
 })
