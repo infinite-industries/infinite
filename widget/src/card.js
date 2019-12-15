453594edffd30fcd203863e72a5952506dfed5ae
dima <shifting.planes@gmail.com>
2019-12-15 14:11:57 -0500

    worked out basic structure of the widget

--- widget/src/card.js
+++ widget/src/card.js
@@ -1,5 +1,23 @@
-export const Card = {
-    Render: function(){
-        console.log("Rendering a card")
-    }
+export function Card (event_data){
+    const template = `
+    <div class="card-container">
+        <div class="card-image">
+
+        </div>
+
+        <div id="title-content" class="card-title">
+            ${event_data.title}
+        </div>
+
+        <div id="text-content" class="card-text">
+
+        </div>
+
+        <div class="card-button-box">
+            <span class="infinite-card-button">activate</span>
+        </div>
+    </div>`
+
+    return template
+
 }
--- widget/src/index.js
+++ widget/src/index.js
@@ -1,16 +1,29 @@
 // Import API helper
 import {APICaller} from './apiCaller.js'
-// Import Card template and card list renderer
+// Import Card template and renderer
 import {Card} from './card.js'
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("begin rendering")
+    console.log("begin injecting widget content")
+    const infinite_widget_container = document.querySelector('#infinite-widget')
 
-    APICaller((events) => {
-        events.forEach((event)=>{
-            console.log("\n-----------\n" + JSON.stringify(event))
-            //content.insertAdjacentHTML('beforeend', Card.Render(event))
+    if(infinite_widget_container !== null){
+        console.log("Loading widget content...")
+
+        const content = document.createElement('div')
+        content.setAttribute("id", "infinite-widget-content")
+        infinite_widget_container.innerHTML = ""
+        infinite_widget_container.appendChild(content)
+
+        APICaller((events) => {
+            events.forEach((event)=>{
+                console.log("\n-----------\n" + JSON.stringify(event))
+                content.insertAdjacentHTML('beforeend', Card(event))
+            })
         })
-    })
+    }
+    else {
+        console.log("Please make sure that element with id \"infinite-widget\" is present in the body of your document.")
+    }
 })
