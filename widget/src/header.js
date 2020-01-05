00de4f5bf3a4808f5cded802167e25046837c447
dima <shifting.planes@gmail.com>
2020-01-05 14:15:54 -0500

    updated tests and evironmental variables

--- widget/src/header.js
+++ widget/src/header.js
@@ -1,4 +1,7 @@
 
+// Jest doesn't use the webpack config so SITE_URL doesn't get defined during tests
+const POWERED_BY_URL = typeof SITE_URL !== 'undefined' ? SITE_URL : 'https://staging.infinite.industries'
+
 export default function Header (event_title){
 
     if(event_title === null){ event_title = ""}
@@ -7,8 +10,8 @@ export default function Header (event_title){
         <div id="infinite-header">
             <h2 style="color:white">${event_title}</h2>
             <div id="branding">
-                <a href="https://infinite.industries" target="_new">
-                    powered by Infinite Indistries
+                <a href="${POWERED_BY_URL}" target="_new">
+                    powered by Infinite Industries
                 </a>
             </div>
         </div>
