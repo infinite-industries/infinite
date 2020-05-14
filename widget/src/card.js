efae4626e9b479894783721b7b482a3b145f476d
Matthew Gidcomb <matthew.gidcomb@outlook.com>
2020-05-13 20:43:55 -0400

    Update Cards to handle new remote and resource events

--- widget/src/card.js
+++ widget/src/card.js
@@ -1,28 +1,56 @@
 import ConfigService from './configService.js'
 import TimeService from './timeService.js'
 
+const MAX_LENGTH_OF_EVENT_TITLE = 35
 const MAX_LENGTH_OF_VENUE_TITLE = 35
 const MAX_LENGTH_OF_EVENT_DESCRIPTION = 120
 
 
-const TitleShortner = function(full_title){
-    if(full_title.length > MAX_LENGTH_OF_VENUE_TITLE) {
-        full_title = full_title.slice(0,MAX_LENGTH_OF_VENUE_TITLE)+"..."
+const TitleShortener = function(full_title, max_length) {
+    if(full_title.length > max_length) {
+        full_title = full_title.slice(0, max_length) + "..."
     }
 
     return full_title
 }
 
+const VenueSection = function(event_data) {
+    if (event_data.venue) {
+        return `<div class="infinite-venue-container">
+                <div>
+                    <h4>
+                        <svg width="20px" height="20px" viewBox="0 0 3.8674 5.5" x="0px" y="0px">
+                            <g :fill="iconColor"><path d="M1.9337 0.0001c1.068,0 1.9337,0.8656 1.9337,1.9336 0,0.7798 -0.7354,1.7041 -1.142,2.4084 -0.2639,0.4571 -0.5279,0.914 -0.7917,1.3711 -0.2638,-0.4571 -0.5278,-0.914 -0.7917,-1.3711 -0.4066,-0.7043 -1.142,-1.6286 -1.142,-2.4084 0,-1.068 0.8657,-1.9336 1.9337,-1.9336zm0 1.1518c0.4319,0 0.7818,0.3499 0.7818,0.7818 0,0.4319 -0.3499,0.7819 -0.7818,0.7819 -0.4319,0 -0.7818,-0.35 -0.7818,-0.7819 0,-0.4319 0.3499,-0.7818 0.7818,-0.7818z" /></g>
+                        </svg>
+                     </h4>
+                </div>
+            <div class="venue-name">
+                <h4>
+                    ${TitleShortener(event_data.venue.name, MAX_LENGTH_OF_VENUE_TITLE)}
+                </h4>
+            </div>
+        </div>`
+
+    } else return ''
+}
+
+const DateTimeSection = function(event_data) {
+    if (event_data.date_times && event_data.date_times.length > 0) {
+        let day = TimeService.returnDay (event_data.date_times[0].start_time)
+        let month = TimeService.returnMonth (event_data.date_times[0].start_time)
+        let date = TimeService.returnDate (event_data.date_times[0].start_time)
+        let start_hour = TimeService.returnHourMinutesAMPM (event_data.date_times[0].start_time)
+        let end_hour = TimeService.returnHourMinutesAMPM (event_data.date_times[0].end_time)
+        return `<div class="infinite-time-container">
+                <p class="date">${day}, ${month} ${date}</p>
+                <p class="time">${start_hour} - ${end_hour}</p>
+            </div>`
+    } else return ''
+}
+
 export default function Card (event_data){
 
-    let processed_brief_description = event_data.brief_description.slice(0,MAX_LENGTH_OF_EVENT_DESCRIPTION)+"..."
-    let day = TimeService.returnDay (event_data.date_times[0].start_time)
-    let month = TimeService.returnMonth (event_data.date_times[0].start_time)
-    let date = TimeService.returnDate (event_data.date_times[0].start_time)
-    let start_hour = TimeService.returnHourMinutesAMPM (event_data.date_times[0].start_time)
-    let end_hour = TimeService.returnHourMinutesAMPM (event_data.date_times[0].end_time)
-
-    let venue_name = TitleShortner(event_data.venue.name)
+    let processed_brief_description = TitleShortener(event_data.brief_description, MAX_LENGTH_OF_EVENT_DESCRIPTION)
 
     const template = `
     <div class="infinite-card">
@@ -32,28 +60,12 @@ export default function Card (event_data){
             </div>
 
             <div class="infinite-title-container">
-                <h3 class="infinite-card-title">${event_data.title}</h3>
+                <h3 class="infinite-card-title">${TitleShortener(event_data.title, MAX_LENGTH_OF_EVENT_TITLE)}</h3>
             </div>
 
-            <div class="infinite-venue-container">
-                <div>
-                    <h4>
-                        <svg width="20px" height="20px" viewBox="0 0 3.8674 5.5" x="0px" y="0px">
-                            <g :fill="iconColor"><path d="M1.9337 0.0001c1.068,0 1.9337,0.8656 1.9337,1.9336 0,0.7798 -0.7354,1.7041 -1.142,2.4084 -0.2639,0.4571 -0.5279,0.914 -0.7917,1.3711 -0.2638,-0.4571 -0.5278,-0.914 -0.7917,-1.3711 -0.4066,-0.7043 -1.142,-1.6286 -1.142,-2.4084 0,-1.068 0.8657,-1.9336 1.9337,-1.9336zm0 1.1518c0.4319,0 0.7818,0.3499 0.7818,0.7818 0,0.4319 -0.3499,0.7819 -0.7818,0.7819 -0.4319,0 -0.7818,-0.35 -0.7818,-0.7819 0,-0.4319 0.3499,-0.7818 0.7818,-0.7818z" /></g>
-                        </svg>
-                    </h4>
-                </div>
-                <div class="venue-name">
-                    <h4>
-                        ${venue_name}
-                    </h4>
-                </div>
-            </div>
+            ${VenueSection(event_data)}
 
-            <div class="infinite-time-container">
-                <p class="date">${day}, ${month} ${date}</p>
-                <p class="time">${start_hour} - ${end_hour}</p>
-            </div>
+            ${DateTimeSection(event_data)}
 
             <div class="infinite-description-container">
                 ${processed_brief_description}
