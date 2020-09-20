687824e65dc47aa746f703c1e0af3af9b49b5844
Matthew Gidcomb <matthew.gidcomb@outlook.com>
2020-09-20 10:32:17 -0400

    Fix linter errors

--- web-portal/pages/donate.vue
+++ web-portal/pages/donate.vue
@@ -3,10 +3,10 @@
     <h1>Donate</h1>
 
     <p>
-        <em>Why we need the funds?</em> We are sustained by a mix of volunteer and paid activities. We try to pay our interns for doing research and outreach tasks and generally behave like solid human beings. We are also very much interested in ramping up our outreach activities to grow our positive impact on folks around us - so in the not-too-distant future, this means hiring a dedicated staff person. Also, occasionally we commission artists to create stickers and other materials we use for promotion.
+      <em>Why we need the funds?</em> We are sustained by a mix of volunteer and paid activities. We try to pay our interns for doing research and outreach tasks and generally behave like solid human beings. We are also very much interested in ramping up our outreach activities to grow our positive impact on folks around us - so in the not-too-distant future, this means hiring a dedicated staff person. Also, occasionally we commission artists to create stickers and other materials we use for promotion.
     </p>
     <p>
-        <a href="https://www.paypal.com/biz/fund?id=GCF56FD8ARSD6" target="_new">Please donate via PayPal</a>
+      <a href="https://www.paypal.com/biz/fund?id=GCF56FD8ARSD6" target="_new">Please donate via PayPal</a>
     </p>
 
   </div>
@@ -19,4 +19,3 @@
     }
   }
 </script>
-
