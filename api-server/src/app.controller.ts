39f59e37d2b56d065bdc378e761a01ea2350ec2a
Chris Wininger <chris.wininger@gmail.com>
2021-07-25 10:10:07 -0400

    Add supported version info
    This adds an array of supported versions to the /version endpoint
    response. This will make it possible for future clients such as mobile
    apps to check if the api version they are targeting is still supported by the
    api.

--- api-server/src/app.controller.ts
+++ api-server/src/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import {ApiOperation, ApiTags} from "@nestjs/swagger";
 import {VersionResponse} from "./dto/version-response";
-import {CURRENT_VERSION_URI} from "./utils/versionts";
+import {CURRENT_VERSION_URI, SUPPORTED_VERSIONS} from "./utils/versionts";
 
 @Controller()
 @ApiTags("API Root")
@@ -12,7 +12,8 @@ export class AppController {
   @ApiOperation({ summary: 'says hello to the world' })
   getVersion(): VersionResponse {
     const version = CURRENT_VERSION_URI;
+    const supportedVersions = SUPPORTED_VERSIONS;
 
-    return { version }
+    return { version, supportedVersions }
   }
 }
