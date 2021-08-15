745fe72cbcfad4c04a479fd80a9b7e8f8fc5564f
Matthew Gidcomb <matthew.gidcomb@outlook.com>
2021-08-15 11:17:04 -0400

    Separate route test process into a script

---
+++ e2e-tests/wait-for-stack.sh
@@ -0,0 +1,20 @@
+# waits for 5 minutes (30 retries on 10-second intervals)
+limit=30
+
+service_trials=0
+
+printf 'wait for api service\n'
+
+until $(curl --output /dev/null --silent --head --fail http://localhost:3003/api/version); do
+    printf "...waiting %s \n" "$service_trials"
+    ((service_trials=service_trials+1))
+
+    if [ $service_trials -gt $limit ]; then
+      printf "ERROR: Timed out waiting for api\n"
+      exit 1
+    fi
+
+    sleep 10
+done
+
+printf '...found service!\n'
