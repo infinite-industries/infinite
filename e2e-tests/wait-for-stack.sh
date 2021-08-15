db8e98689cca57689766e391aeb04e924d549494
Matthew Gidcomb <matthew.gidcomb@outlook.com>
2021-08-15 11:40:05 -0400

    Also try waiting for the portal

--- e2e-tests/wait-for-stack.sh
+++ e2e-tests/wait-for-stack.sh
@@ -19,3 +19,21 @@ until $(curl --output /dev/null --silent --head --fail http://localhost:3003/ver
 done
 
 printf '...found service!\n'
+# troubleshooting -- ensure the API server is _actually_ up, and not spitting back an error
+curl --silent --fail http://localhost:3003/version
+
+# now try the web portal
+service_trials=0
+printf "wait for web portal\n"
+until $(curl --output /dev/null --silent --head --fail http://localhost:7779); do
+    printf "...waiting %s\n" "$service_trials"
+    ((service_trials=service_trials+1))
+    if [ $service_trials -gt $limit ]; then
+        printf "ERROR: Timed out waiting for web portal\n"
+        exit 1
+    fi
+
+    sleep $interval
+done
+
+printf '...found portal!\n'
