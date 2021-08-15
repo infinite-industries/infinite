70ddcb0f3ddd5b8cbb9cde67c427cf98e21df608
Matthew Gidcomb <matthew.gidcomb@outlook.com>
2021-08-15 11:19:47 -0400

    Oops that's the wrong endpoint

--- e2e-tests/wait-for-stack.sh
+++ e2e-tests/wait-for-stack.sh
@@ -5,7 +5,7 @@ service_trials=0
 
 printf 'wait for api service\n'
 
-until $(curl --output /dev/null --silent --head --fail http://localhost:3003/api/version); do
+until $(curl --output /dev/null --silent --head --fail http://localhost:3003/version); do
     printf "...waiting %s \n" "$service_trials"
     ((service_trials=service_trials+1))
 
