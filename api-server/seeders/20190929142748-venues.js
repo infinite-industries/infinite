857d03f968a90a1fc314abbf7fea9804e8e8a26c
Chris Wininger <cwininger@sonatype.com>
2021-08-22 11:51:28 -0400

    fix seeder

--- api-server/seeders/20190929142748-venues.js
+++ api-server/seeders/20190929142748-venues.js
@@ -1,19 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const faker = require('faker');
 const numVenues = 10;
+const uuidv4 = require('uuid').v4;
 
 module.exports = {
   up: (queryInterface) => {
     let venues = [];
 
     for (let i = 0; i < numVenues; i++) {
-      const venue = new venueModel({
+      const venue ={
         id: uuidv4(),
         name: faker.company.companyName(),
         slug: faker.lorem.slug(),
         address: faker.address.streetAddress(),
         g_map_link: faker.internet.url()
-      });
+      };
 
       venues = [...venues, venue];
     }
