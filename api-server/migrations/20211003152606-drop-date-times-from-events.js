13138b82c54c28c95b433ee363dfbde9a78c402a
Rebecca Mead <beckyleemead@gmail.com>
2021-10-10 12:41:28 -0400

    add reverting functionality to recreate date_times column in events table

--- api-server/migrations/20211003152606-drop-date-times-from-events.js
+++ api-server/migrations/20211003152606-drop-date-times-from-events.js
@@ -6,11 +6,23 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    return queryInterface.sequelize.query(`
+      ALTER TABLE events
+      ADD COLUMN date_times jsonb;
+
+      UPDATE events
+      SET date_times = date_times_agg
+      FROM
+      (SELECT
+        event_id,
+        jsonb_agg(
+          json_build_object('start_time', start_time, 'end_time', end_time, 'optional_title', optional_title)
+          ORDER BY start_time)
+        AS date_times_agg
+        FROM datetime_venue
+        GROUP BY event_id
+        ) agg
+      WHERE events.id = agg.event_id;
+    `)
   }
 };
