116a284450d1d95590a6d21f5f253285b1919fa1
Chris Wininger <cwininger@sonatype.com>
2021-10-17 11:43:40 -0400

    Add option to use ssl on db connection (#251)
    * Added added an env variable and supported code allow use with an ssl
      enabled database instance
    
    * Added env flag to .env.sample/created .env.sample for the api-server
    
    * Consolidated migration config code

--- api-server/config/config.js
+++ api-server/config/config.js
@@ -1,29 +1,31 @@
 require('dotenv').config();
 
+const isUsingSSL = (process.env.SQL_IS_USING_SSL || '').toLowerCase() === 'true';
+
+const dialectOptions = isUsingSSL
+    ?
+    {
+      ssl: {
+        require: true
+      }
+    }
+    : undefined;
+
+
+const options = {
+  //logging: console.log, # Uncomment this to output
+  username: process.env.DB_USER_NAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: "postgres",
+  ssl: isUsingSSL,
+  dialectOptions
+}
+
 module.exports = {
-  "development": {
-    //logging: console.log, # Uncomment this to output
-    "username": process.env.DB_USER_NAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": "postgres"
-  },
-  "test": {
-    "username": process.env.DB_USER_NAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": "postgres"
-  },
-  "production": {
-    "username": process.env.DB_USER_NAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
-    "port": process.env.DB_PORT,
-    "dialect": "postgres"
-  }
+  development: options,
+  test: options,
+  production: options
 }
