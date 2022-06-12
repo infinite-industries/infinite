4f7948af2a05bfb25f12c78670c83d4ebcbad67c
Chris Wininger <chris.wininger@gmail.com>
2022-06-12 13:53:39 -0400

    foo

--- api-server/docker-build.sh
+++ api-server/docker-build.sh
@@ -2,13 +2,15 @@
 
 set -e
 
+tag="${1:=local}"
+
 script_name="docker-build"
 container_name="infinite-api-server"
 
-echo "$script_name: building $container_name"
+echo "$script_name: building $container_name for tag $tag"
 
-docker build -t "$container_name:latest" .
+docker build -t "$container_name:$tag" .
 
-echo "$script_name: finished building $container_name"
+echo "$script_name: finished building $container_name for tag $tag"
 
 set +e
