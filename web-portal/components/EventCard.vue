c289879fe839e48b9fa2c07951ecd0c80bad1a75
Chris Wininger <chris.wininger@gmail.com>
2022-09-18 12:48:24 -0400

    Fix e-mail filter
    organizer contact should be filtered out if the user accessing them is
    not an admin. This filter needs to be applied to any endpoint that
    allows non-authenticated access.
    
    To get this working a bit of tech debt had to be payed down. It was the
    expectation that we would return an EventDTO model over the wire rather
    than the database model. This gives us space to normalize the format for
    the api rather than except whatever format sequelize returns, for
    example we currently flatten venues to venue since we only allow one
    venue right now and we do some normalization of data/time values.
    
    This transformation was not getting applied everywhere but our our
    filter functions expected the transformed type.
    
    This PR:
    
    1. Makes sure all even endpoints expose the proper DTO
    2. Applies organizer_contact filter to all endpoints that don't require
       authentication
    3. Updates frontend code that wasn't expecting the venue field to
       already be set
    
    Notes:
    
    * If you hit an endpoint that does not require authentication but
    you are authenticated as a site admin the filter will not be applied and
    you will get back the organizer_contact.
    * We don't bother invoking the filter at all on endpoints that require
      authentication as we don't have any non-admin user roles at the
      moment. If you are authenticated you are admin so even if we applied
      the filtering code it would see that your are an admin and doing
      nothing.

--- web-portal/components/EventCard.vue
+++ web-portal/components/EventCard.vue
@@ -103,22 +103,7 @@
         return !!this.calendar_event.venue_id && (!!this.calendar_event.venue || (!!this.venue && !!this.venue.id))
       },
       venue: function () {
-        // venues should be included on event
-        // if not, the event may have been fetched without the proper flag set,
-        // in which case we may be able to silently recover by consulting a
-        // global list of venues
-        const event = this.calendar_event
-        if (event.venues && event.venues.length > 0) {
-          return event.venues[0]
-        } else if (event.venue_id) {
-          const all_venues = this.$store.getters.GetActiveVenues
-
-          const current_venue = all_venues.find((venue) => {
-            return venue.id === event.venue_id
-          })
-
-          return current_venue === undefined ? {} : current_venue
-        } else return {}
+        return this.calendar_event.venue || {}
       },
       showTime: function () {
         return !_hasTag(this.calendar_event, 'category:online-resource') &&
@@ -158,6 +143,10 @@
     },
     filters: {
       truncate: function (text, stop, clamp) {
+        if (text === undefined || text === null) {
+          return ''
+        }
+
         return text.slice(0, stop) + (stop < text.length ? clamp || '...' : '')
       }
     },
