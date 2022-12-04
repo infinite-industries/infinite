668d4b9a71a09d8de80e361946a65bd020fdb9e4
Matthew Gidcomb <matthew.gidcomb@outlook.com>
2022-12-04 10:48:30 -0500

    One more fix for canonical links

--- web-portal/plugins/page-meta-plugin.js
+++ web-portal/plugins/page-meta-plugin.js
@@ -1,5 +1,5 @@
 export default ({ app }, inject) => {
   inject('urlFor', function urlFor(route) {
-    return `${app.$config.APP_URL}${route}}`
+    return `${app.$config.APP_URL}${route}`
   })
 }
