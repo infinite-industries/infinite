0dc40cca1ecd88fc330fe472a2ffe216ba5758f7
Chris Wininger <chris.wininger@gmail.com>
2023-02-19 10:40:23 -0500

    Updating docker-compose to env tag
    Updating the docker-compose deployed via ansible to derive tags from
    host environment
    
    Updated playbook and run scripts to set tags correctly in the host
    environment of each vm

--- ansible/deploy-site_local.sh
+++ ansible/deploy-site_local.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 INFINITE_DEPLOY_ENV='local' \
+INFINITE_IMAGE_VERSION_TAG='local' \
 ansible-playbook --extra-vars "ansible_sudo_pass=xxx" ./deploy_site_playbook.yml
--- ansible/deploy-site_staging.sh
+++ ansible/deploy-site_staging.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 INFINITE_DEPLOY_ENV='staging' \
+INFINITE_IMAGE_VERSION_TAG='development' \
 ansible-playbook ./deploy_site_playbook.yml
