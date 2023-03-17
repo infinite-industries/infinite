0a041d4901fb6979936597aeb1e5fffa109a8908
Jason Swank <jswank@sonatype.com>
2023-03-16 22:11:18 -0400

    Initial revision with secret info & templates.

--- ansible/deploy-site_local.sh
+++ ansible/deploy-site_local.sh
@@ -2,6 +2,5 @@
 
 # This is used for deploying to local vms for testing
 
-INFINITE_DEPLOY_ENV='local' \
-INFINITE_IMAGE_VERSION_TAG='development' \
-ansible-playbook --extra-vars "ansible_sudo_pass=xxx" ./deploy_site_playbook.yml
+ansible-playbook -l local ./deploy_site_playbook.yml
+
