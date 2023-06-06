921c3b63f8aa2f5bfa04baad6e4525241adda5f2
Jason Swank <jswank@scalene.net>
2023-06-06 15:37:13 -0400

    move docker images to ghcr.io

--- api-server/docker-build.sh
+++ api-server/docker-build.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+# An alternative to using this script is to use a just recipe:
+#
+# $ just name=infinite-industries/api-server tag=local build
+#
+# An alternative alternative is to make use of a .env file and just:
+#
+# $ printf "IMAGE_NAME=infinite-industries/api-server\nIMAGE_TAG=local" > .env
+# $ just build
 set -e
 
 tag="${1:-local}"
 
 script_name="docker-build"
-container_name="infinite-api-server"
+container_name="infinite-industries/api-server"
 
 echo "$script_name: building $container_name for tag $tag"
 
