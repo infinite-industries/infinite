c664d430f1822ea8e3a03b343f1009e32228f9fd
Matthew Gidcomb <matthew.gidcomb@outlook.com>
2023-06-11 10:14:58 -0400

    Wire in Fathom using fathom-client
    Includes a proof-of-concept event/goal tracking instance on subscribe page.

--- web-portal/plugins/analytics.client.js
+++ web-portal/plugins/analytics.client.js
@@ -1,38 +1,62 @@
+import {
+  blockTrackingForMe,
+  enableTrackingForMe,
+  load,
+  trackGoal,
+  trackPageview
+} from 'fathom-client'
+
 /**
  * Central place for enabling analytics suites
  */
 export default ({ $config }, inject) => {
   const FATHOM_SITE_ID = $config.FATHOM_SITE_ID
-  console.log(FATHOM_SITE_ID)
   inject('analytics', new Analytics(FATHOM_SITE_ID))
 }
 
 class Analytics {
   constructor(fathomSiteId) {
     this.siteId = fathomSiteId
-    // TODO: load Fathom (consider https://npmjs.com/package/fathom-client)
-    // TODO: how to short-circuit here if ID not provided?
-  }
-
-  trackPageView (...args) {
-    // TBD
-  }
-
-  trackGoal (...args) {
-    // TBD
-  }
-
-  enable () {
-    // TBD
-  }
-
-  disable () {
-    // TBD
-  }
-
-  setConfig ({ fathomSiteId }) {
     if (fathomSiteId) {
-      // TBD
+      load(fathomSiteId, {
+        spa: 'auto'
+      })
+    }
+  }
+
+  /**
+   * Manually track a page view (you probably don't need to do this)
+   */
+  trackPageView (...args) {
+    if (this.siteId) {
+      trackPageview(...args)
+    }
+  }
+
+  /**
+   * Track a specific event e.g. clicks on a particular button
+   */
+  trackEvent (goal, value) {
+    if (this.siteId) {
+      trackGoal(goal, value)
+    }
+  }
+
+  /**
+   * Enable analytics for this session (persisted to LocalStorage)
+   */
+  enable () {
+    if (this.siteId) {
+      enableTrackingForMe()
+    }
+  }
+
+  /**
+   * disable analytics for this user (persisted to LocalStorage)
+   */
+  disable () {
+    if (this.siteId) {
+      blockTrackingForMe()
     }
   }
 }
