b2f2f7c390a4ad4c5a1de63796748d2596cdfbc4
Matthew Gidcomb <matthew.gidcomb@outlook.com>
2023-07-09 10:38:31 -0400

    More structured data improvements
    - event "attendance mode" (maps well to our mode)
    - venue hasMap property to get our Google Maps links in there
    - subjectOf to include URLs

--- web-portal/services/JsonLdService.js
+++ web-portal/services/JsonLdService.js
@@ -1,3 +1,6 @@
+
+const SCHEMA = 'https://schema.org'
+
 export default class JsonLdService {
   static forEvent(event) {
     if (!event) return null
@@ -18,6 +21,8 @@ export default class JsonLdService {
       ? 'Cancelled'
       : (tags.includes('condition:postponed') ? 'Postponed' : null)
 
+    const soldOut = tags.includes('condition:sold-out')
+
     const eventMode = tags.find(t => t.startsWith('mode:'))?.split(':')[1]
 
     // if something doesn't have times, it's not an Event for purposes
@@ -26,21 +31,33 @@ export default class JsonLdService {
     if (!firstStartTime) return null
 
     return {
-      '@context': 'http://schema.org',
+      '@context': SCHEMA,
       '@type': 'Event',
       'name': title,
       'description': description,
       'startDate': firstStartTime,
       'endDate': lastEndTime,
       'image': image,
+      'eventAttendanceMode': `${SCHEMA}/${eventMode === 'in-person' ? 'Offline' : eventMode === 'online' ? 'Online' : 'Mixed'}EventAttendanceMode`,
       ...(venue ? { 'location': this.forVenue(venue, eventMode) } : null),
-      ...(eventStatus ? { eventStatus: `https://schema.org/Event${eventStatus}` } : null),
+      ...(eventStatus ? { eventStatus: `${SCHEMA}/Event${eventStatus}` } : null),
       ...(event.ticket_link || event.eventbrite_link
         ? {
             'offers': {
               '@type': 'Offer',
-              'url': event.ticket_link || event.eventbrite_link
+              'url': event.ticket_link || event.eventbrite_link,
+              'availability': `${SCHEMA}/${soldOut ? 'SoldOut' : 'InStock'}`
             }
+          }
+        : null),
+      ...(event.website_link || event.ticket_link || event.fb_event_link || event.eventbrite_link
+        ? {
+            'subjectOf': [
+              event.website_link ? this.subjectForUrl(event.website_link, 'Event Website') : null,
+              event.ticket_link ? this.subjectForUrl(event.ticket_link, 'Ticket Link') : null,
+              event.fb_event_link ? this.subjectForUrl(event.fb_event_link, 'Facebook Event') : null,
+              event.eventbrite_link ? this.subjectForUrl(event.ticket_link, 'Eventbrite Link') : null
+            ].filter(s => !!s)
           }
         : null)
     }
@@ -67,7 +84,16 @@ export default class JsonLdService {
       'name': venue.name,
       // TODO: how to figure out URL -- may need to add to data model
       // 'sameAs': '????',
-      ...(type === 'Place' ? { 'address': address } : null)
+      ...(type === 'Place' ? { 'address': address } : null),
+      ...(type === 'Place' && venue.g_map_link ? { 'hasMap': venue.g_map_link } : null)
+    }
+  }
+
+  static subjectForUrl(url, name) {
+    return {
+      '@type': 'WebPage',
+      url,
+      name
     }
   }
 }
