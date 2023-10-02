fa8372201bb42332912e648f8c3af995239218e6
Chris Wininger <cwininger@sonatype.com>
2023-10-02 11:49:17 -0400

    mute sequelize logs unless asked for in tests

--- api-server/src/db_constants.js
+++ api-server/src/db_constants.js
@@ -7,7 +7,9 @@
 require('dotenv').config();
 
 // === Database and Sequelize ===
-module.exports.SEQUELIZE_LOGGING = !!process.env.SEQUELIZE_LOGGING;
+module.exports.SEQUELIZE_LOGGING =
+  !!process.env.SEQUELIZE_LOGGING &&
+  process.env.SEQUELIZE_LOGGING.trim().length !== 0;
 module.exports.DEBUG_MIGRATION = isTrueFromString(process.env.DEBUG_MIGRATION);
 
 // === Database Connection ===
@@ -15,8 +17,10 @@ module.exports.DB_USER_NAME = process.env.DB_USER_NAME || 'postgres';
 module.exports.DB_PASSWORD = process.env.DB_PASSWORD || 'xxx';
 module.exports.DB_NAME = process.env.DB_NAME || 'infinite-api';
 module.exports.DB_PORT = !!process.env.DB_PORT ? +process.env.DB_PORT : 5436;
-module.exports.DB_HOST= process.env.DB_HOST || 'localhost';
-module.exports.SQL_IS_USING_SSL = isTrueFromString(process.env.SQL_IS_USING_SSL);
+module.exports.DB_HOST = process.env.DB_HOST || 'localhost';
+module.exports.SQL_IS_USING_SSL = isTrueFromString(
+  process.env.SQL_IS_USING_SSL,
+);
 
 // === These values are used when starting postgres for running tests and are passed along to sequelize, they will typically be set by DOCKER env not .env ===
 module.exports.POSTGRES_DB = process.env.POSTGRES_DB;
