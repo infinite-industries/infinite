b03bc46ea3865f950d520f0054a29e363b324ccd
Chris Wininger <cwininger@sonatype.com>
2024-04-14 16:45:28 -0400

    nearly working

--- web-portal/components/Pagination.vue
+++ web-portal/components/Pagination.vue
@@ -3,25 +3,45 @@
     <slot v-bind="page" />
     <v-pagination v-model="pageNumber" :length="pageCount" />
 
+    <!--    !!! page.label is a bad key, it can happen more than once, should create a key field, that's number prev or next-->
     <ul class="ii-pagination__list">
       <li
         v-for="page in visiblePageLinks"
-        :key="page.pageNumber"
+        :key="page.label"
         class="ii-pagination__entry"
       >
         <button
           :class="getLinkClasses(page)"
-          v-if="!page.isSeparator"
+          v-if="page.entryType === 'page-number'"
           :disabled="!page.enabled"
+          @click="setPage(page.pageNumber)"
         >
-          {{ page.pageNumber }}
+          {{ page.label }}
+        </button>
+
+        <button
+          :class="getLinkClasses(page)"
+          v-if="page.entryType === 'previous'"
+          :disabled="!page.enabled"
+          @click="decrementPage()"
+        >
+          {{ page.label }}
+        </button>
+
+        <button
+          :class="getLinkClasses(page)"
+          v-if="page.entryType === 'next'"
+          :disabled="!page.enabled"
+          @click="incrementPage()"
+        >
+          {{ page.label }}
         </button>
 
         <span
           class="ii-pagination__entry-separator"
           v-if="page.isSeparator"
         >
-          {{ page.pageNumber }}
+          {{ page.label }}
         </span>
       </li>
     </ul>
@@ -62,44 +82,61 @@
         return items && items.length > 0 ? Math.ceil(items.length / pageSize) : 0
       },
 
-      visiblePageLinks: function() {
-        const maxLinks = 10
+      maxLinks: function () {
+        if (process.client) {
+          console.log('!!! client side: ' + window.innerWidth)
+        }
 
-        const visiblePages = [this.createPageEntry('<', this.pageNumber !== 1)]
+        console.log('!!! computing')
+        if (!process.client) {
+          console.log('!!! server side')
+          return 10
+        } else if (window.innerWidth > 700) {
+          return 25
+        } else if (window.innerWidth < 300) {
+          return 2
+        } else {
+          return 10
+        }
+      },
+
+      visiblePageLinks: function() {
+        const maxLinks = this.maxLinks
+
+        const visiblePages = [this.createPreviousEntry(this.pageNumber !== 1)]
 
         if (this.pageCount < maxLinks) {
           // just show everything
           for (let i = 1; i <= this.pageCount; i++) {
-            visiblePages.push(this.createPageEntry(`${i}`, this.pageNumber !== i))
+            visiblePages.push(this.createPageEntry(i, this.pageNumber !== i))
           }
         } else if (this.pageNumber <= maxLinks) {
           // too many to show everything, current is is less that number of links
           // we will show
           for (let i = 1; i <= maxLinks; i++) {
-            visiblePages.push(this.createPageEntry(`${i}`, this.pageNumber !== i))
+            visiblePages.push(this.createPageEntry(i, this.pageNumber !== i))
           }
 
           if (maxLinks < this.pageCount) {
             visiblePages.push(this.createPageEntry('...'))
           }
 
-          visiblePages.push(this.createPageEntry(`${this.pageCount}`))
+          visiblePages.push(this.createPageEntry(this.pageCount))
         } else {
           // the current page is is greater than number of links shown, so make
           // sure to display first page, ..., and current through number shown
-          visiblePages.push(this.createPageEntry('1'))
+          visiblePages.push(this.createPageEntry(1))
           if (maxLinks < this.pageCount) {
             visiblePages.push(this.createPageEntry('...'))
           }
 
-          console.log('!!! here')
           if (this.pageNumber + maxLinks < this.pageCount) {
             for (let i = this.pageNumber; i <= this.pageNumber + maxLinks; i++) {
-              visiblePages.push(this.createPageEntry(`${i}`, this.pageNumber !== i))
+              visiblePages.push(this.createPageEntry(i, this.pageNumber !== i))
             }
           } else {
             for (let i = this.pageCount - maxLinks; i <= this.pageCount; i++) {
-              visiblePages.push(this.createPageEntry(`${i}`, this.pageNumber !== i))
+              visiblePages.push(this.createPageEntry(i, this.pageNumber !== i))
             }
           }
 
@@ -107,17 +144,36 @@
           if (this.pageNumber + maxLinks < this.pageCount) {
             visiblePages.push(this.createPageEntry('...'))
 
-            visiblePages.push(this.createPageEntry(`${this.pageCount}`))
+            visiblePages.push(this.createPageEntry(this.pageCount))
           }
         }
 
-        visiblePages.push(this.createPageEntry('>', this.pageNumber !== this.pageCount))
+        visiblePages.push(this.createNextEntry(this.pageNumber !== this.pageCount))
 
         return visiblePages
       }
     },
     methods: {
-      createPageEntry(pageNumber, enabled) {
+      createPreviousEntry(enabled) {
+        return {
+          entryType: 'previous',
+          entrykey: 'previous',
+          enabled,
+          label: '<'
+        }
+      },
+
+      createNextEntry(enabled) {
+        return {
+          entryType: 'next',
+          entryKey: 'next',
+          label: '>',
+          enabled
+        }
+      },
+      createPageEntry(label, enabled) {
+        const pageNumber = isNaN(label) ? null : `${label}`
+
         const isSeparator = pageNumber === '...'
         const isBookEnd = pageNumber === '>' || pageNumber === '<'
         if (enabled === null || enabled === undefined) {
@@ -125,11 +181,25 @@
         }
 
         return {
+          entryType: isSeparator ? 'separator' : 'page-number',
+          label,
           pageNumber,
           isSeparator,
           isBookEnd,
-          enabled
+          enabled,
+          entryKey: isSeparator ? 'Separator' : label
         }
+      },
+      setPage(newPageNumber) {
+        if (newPageNumber > 0 && newPageNumber <= this.pageCount) {
+          this.pageNumber = parseInt(newPageNumber)
+        }
+      },
+      incrementPage() {
+        this.setPage(this.pageNumber + 1)
+      },
+      decrementPage() {
+        this.setPage(this.pageNumber - 1)
       },
       getLinkClasses(pageEntry) {
         const classes = ['ii-pagination__entry-button']
