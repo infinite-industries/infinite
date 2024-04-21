8749d9132d0276df830dfe99f7220bc8a33d80a8
Chris Wininger <cwininger@sonatype.com>
2024-04-21 13:31:06 -0400

    refining tests

--- web-portal/components/pagination/Pagination.vue
+++ web-portal/components/pagination/Pagination.vue
@@ -70,21 +70,7 @@
     },
     data: function () {
       return {
-        pageNumber: 1,
-        windowWidth: null
-      }
-    },
-    mounted() {
-      if (window) {
-        window.addEventListener('resize', this.windowResized)
-        this.windowWidth = window.innerWidth
-      } else {
-        this.windowWidth = null
-      }
-    },
-    destroyed() {
-      if (window) {
-        window.removeEventListener('resize', this.windowResized)
+        pageNumber: 1
       }
     },
     computed: {
@@ -115,16 +101,21 @@
       },
 
       visiblePageLinks: function() {
+        console.log('!!! compute visiblePageLinks: ' + this.items.length)
         const maxLinks = this.maxNumberOfPageShortcuts // this.maxLinks
 
+        console.log('!!! maxLinks: ' + maxLinks)
+        console.log('!!! pageCount: ' + this.pageCount)
         const visiblePages = [this.createPreviousEntry(this.pageNumber !== 1)]
 
-        if (this.pageCount < maxLinks) {
+        if (this.pageCount <= maxLinks) {
+          console.log('!!! just show everything')
           // just show everything
           for (let i = 1; i <= this.pageCount; i++) {
             visiblePages.push(this.createPageEntry(i, this.pageNumber !== i))
           }
         } else if (this.pageNumber <= maxLinks) {
+          console.log('!!! second condition')
           // too many to show everything, current is is less that number of links
           // we will show
           for (let i = 1; i <= maxLinks; i++) {
--- web-portal/test/Pagination.spec.js
+++ web-portal/test/Pagination.spec.js
@@ -1,9 +1,8 @@
 import { createLocalVue, shallowMount } from '@vue/test-utils'
-import Vuetify from 'vuetify'
 import Pagination from '@/components/pagination/Pagination.vue'
+import expect from 'expect'
 
 const localVue = createLocalVue()
-localVue.use(Vuetify)
 
 describe('Pagination component', () => {
   let wrapper
@@ -52,4 +51,96 @@ describe('Pagination component', () => {
 
     expect(wrapper.vm.pageNumber).toEqual(1)
   })
+
+  it('should show links for all numbers when there are less pages than max number of links shown', async () => {
+    console.log('!!! start')
+    await wrapper.setProps({
+      items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      maxNumberOfPageShortcuts: 10,
+      pageSize: 1
+    })
+
+    assertPaginationListDrawnCorrect({ wrapper, numExpectedLinks: 10 })
+  })
+
+  it('should show truncation spanner (...) for latter half of list when page selection near the beginning', async () => {
+    console.log('!!! start')
+    await wrapper.setProps({
+      items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      maxNumberOfPageShortcuts: 2,
+      pageSize: 1
+    })
+
+    const paginationButtonList = wrapper.find('.ii-pagination__list')
+    expect(paginationButtonList.exists()).toBe(true)
+    const paginationButtons = paginationButtonList.findAll('.ii-pagination__entry-button')
+
+    // prev, page 1, page 2, ..., last page, next
+    expect(paginationButtons.length).toEqual(5)
+    assertHasBookends(paginationButtons)
+    expect(paginationButtons.at(1).text()).toEqual('1')
+    expect(paginationButtons.at(2).text()).toEqual('2')
+
+    // should show span just before last page
+    const paginationEntries = paginationButtonList.findAll('.ii-pagination__entry')
+    expect(paginationEntries.length).toEqual(6)
+    expect(paginationEntries.at(paginationEntries.length - 3).text()).toEqual('...')
+
+    console.log('!!! paginationButtonList: ' + paginationButtonList.html())
+  })
+
+  it('should show truncation spanner (...) for first half of list when page selection near the end', async () => {
+    console.log('!!! start')
+    await wrapper.setProps({
+      items: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      maxNumberOfPageShortcuts: 2,
+      pageSize: 1
+    })
+
+    await wrapper.setData({
+      pageNumber: 8
+    })
+
+    const paginationButtonList = wrapper.find('.ii-pagination__list')
+    expect(paginationButtonList.exists()).toBe(true)
+    const paginationButtons = paginationButtonList.findAll('.ii-pagination__entry-button')
+
+    console.log('!!! paginationButtonList: ' + paginationButtonList.html())
+
+    // prev, page 1, ..., page 8, page 9, last page, next
+    expect(paginationButtons.length).toEqual(6)
+    assertHasBookends(paginationButtons)
+    expect(paginationButtons.at(1).text()).toEqual('1')
+    expect(paginationButtons.at(2).text()).toEqual('2')
+
+    // should show span just before last page
+    const paginationEntries = paginationButtonList.findAll('.ii-pagination__entry')
+    expect(paginationEntries.length).toEqual(6)
+    expect(paginationEntries.at(2).text()).toEqual('...')
+  })
+
+  function assertPaginationListDrawnCorrect({ wrapper, numExpectedLinks }) {
+    const paginationButtonList = wrapper.find('.ii-pagination__list')
+    expect(paginationButtonList.exists()).toBe(true)
+
+    const paginationButtons = paginationButtonList.findAll('.ii-pagination__entry-button')
+    expect(paginationButtons.length).toEqual(numExpectedLinks + 2) // number of pages + prev and next
+
+    assertHasBookends(paginationButtons)
+
+    // check explicit page number links shown
+    for (let i = 1; i <= numExpectedLinks; i++) {
+      expect(paginationButtons.at(i).text()).toEqual(`${i}`)
+    }
+  }
+
+  function assertHasBookends(paginationButtons) {
+    // check previous bookend shown
+    expect(paginationButtons.at(0).text()).toEqual('<')
+
+    // check last bookend shown
+    expect(paginationButtons.at(paginationButtons.length - 1).text()).toEqual('>')
+  }
+
+  // todo test current link disabled, threads options className, test with two spans
 })
