0edae6718081bc1b093c1d40e7074a4a5b0f1233
Chris Wininger <cwininger@sonatype.com>
2024-04-25 17:04:44 -0400

    move the pagination list of buttons into sub component

--- web-portal/components/pagination/Pagination.vue
+++ web-portal/components/pagination/Pagination.vue
@@ -3,25 +3,21 @@
     <slot v-bind="page" />
 
     <div class="ii-pagination__list-wrapper">
-      <ul :class="pageLinkListClassNames">
-        <ii-pagination-list-entry
-          v-for="pageEntry in visiblePageLinks"
-          :key="pageEntry.entryKey"
-          :label="pageEntry.label"
-          :entry-type="pageEntry.entryType"
-          :is-selected="pageEntry.label && pageEntry.label === `${pageNumber}`"
-          :is-disabled="!pageEntry.enabled"
-          @decrementPageClicked="decrementPage()"
-          @incrementPage="incrementPage()"
-          @pageEntryClicked="setPage(...arguments)"
-        />
-      </ul>
+      <ii-pagination-list
+        :class-name-page-list="classNamePageList"
+        :max-number-of-page-shortcuts="maxNumberOfPageShortcuts"
+        :page-number="pageNumber"
+        :page-count="pageCount"
+        @decrementPageClicked="decrementPage()"
+        @incrementPage="incrementPage()"
+        @pageEntryClicked="setPage(...arguments)"
+      />
     </div>
   </div>
 </template>
 
 <script>
-  import PagenationListEntry from '@/components/pagination/PagenationListEntry.vue'
+  import PaginationList from '@/components/pagination/PaginationList.vue'
 
   export default {
     props: {
@@ -39,7 +35,7 @@
       },
       classNamePageList: {
         type: String,
-        default: null
+        default: ''
       }
     },
     data: function () {
@@ -62,108 +58,9 @@
         const items = this.items
         const pageSize = this.pageSize
         return items && items.length > 0 ? Math.ceil(items.length / pageSize) : 0
-      },
-
-      pageLinkListClassNames() {
-        const classes = ['ii-pagination__list']
-
-        if (typeof this.classNamePageList === 'string') {
-          classes.push(this.classNamePageList)
-        }
-
-        return classes.join(' ')
-      },
-
-      visiblePageLinks: function() {
-        const maxLinks = this.maxNumberOfPageShortcuts // this.maxLinks
-
-        const visiblePages = [this.createPreviousEntry(this.pageNumber !== 1)]
-
-        if (this.pageCount <= maxLinks) {
-          // just show everything
-          for (let i = 1; i <= this.pageCount; i++) {
-            visiblePages.push(this.createPageEntry(i, this.pageNumber !== i))
-          }
-        } else if (this.pageNumber <= maxLinks) {
-          // too many to show everything, current is less that number of links
-          // we will show
-          for (let i = 1; i <= maxLinks; i++) {
-            visiblePages.push(this.createPageEntry(i, this.pageNumber !== i))
-          }
-
-          if (maxLinks < this.pageCount) {
-            visiblePages.push(this.createTruncationEntry('truncationEntry1'))
-          }
-
-          // make sure to show a link to the last page
-          visiblePages.push(this.createPageEntry(this.pageCount, true))
-        } else {
-          // the current page is greater than number of links shown, so make
-          // sure to display first page, ..., and current through number shown
-          visiblePages.push(this.createPageEntry(1, true))
-          if (maxLinks < this.pageCount) {
-            visiblePages.push(this.createTruncationEntry('truncationEntry2'))
-          }
-
-          if (this.pageNumber + maxLinks < this.pageCount) {
-            for (let i = this.pageNumber; i <= this.pageNumber + maxLinks - 1; i++) {
-              visiblePages.push(this.createPageEntry(i, this.pageNumber !== i))
-            }
-          } else {
-            for (let i = this.pageCount - maxLinks; i <= this.pageCount; i++) {
-              visiblePages.push(this.createPageEntry(i, this.pageNumber !== i))
-            }
-          }
-
-          // add ... and last page if last page is not already shown
-          if (this.pageNumber + maxLinks < this.pageCount) {
-            visiblePages.push(this.createTruncationEntry('truncationEntry3'))
-
-            visiblePages.push(this.createPageEntry(this.pageCount, true))
-          }
-        }
-
-        visiblePages.push(this.createNextEntry(this.pageNumber !== this.pageCount))
-
-        return visiblePages
       }
     },
     methods: {
-      createPreviousEntry(enabled) {
-        return {
-          entryType: 'previous',
-          label: '<',
-          entryKey: 'previous',
-          enabled
-        }
-      },
-
-      createNextEntry(enabled) {
-        return {
-          entryType: 'next',
-          label: '>',
-          entryKey: 'next',
-          enabled
-        }
-      },
-
-      createTruncationEntry(entryKey) {
-        return {
-          entryType: 'truncation',
-          label: '...',
-          entryKey: entryKey,
-          enabled: false
-        }
-      },
-
-      createPageEntry(pageNumber, enabled) {
-        return {
-          entryType: 'page-number',
-          label: `${pageNumber}`,
-          entryKey: `${pageNumber}`,
-          enabled
-        }
-      },
       setPage(newPageNumber) {
         if (newPageNumber > 0 && newPageNumber <= this.pageCount) {
           this.pageNumber = parseInt(newPageNumber)
@@ -177,7 +74,7 @@
       }
     },
     components: {
-      'ii-pagination-list-entry': PagenationListEntry
+      'ii-pagination-list': PaginationList
     },
     watch: {
       items: function (newItems, oldItems) {
@@ -188,14 +85,3 @@
     }
   }
 </script>
-
-<style scoped>
-  .ii-pagination__list {
-    align-items: center;
-    display: inline-flex;
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    max-width: 100%;
-  }
-</style>
