097ee3780351f99237c8d9e8c542be7123fc7657
Matthew Gidcomb <matthew.gidcomb@outlook.com>
2024-06-23 19:00:20 -0400

    First pass at tests for JSON-LD metadata service (#505)

---
+++ e2e-tests/integration/eventMetadataTest.js
@@ -0,0 +1,10 @@
+// proposal:
+// this suite should
+// (1) set up an event similar to the way eventEditingTest does
+// (2) load up the single-event page (/events/:id) and make assertions
+// and its primary mission should be to ensure the metadata in the head
+// (title and description, Open Graph, JSON-LD) is correct
+// given that we have broken parts of this in the past, this could be
+// medium-to-high value
+
+context.skip('Event metadata', () => {});
--- web-portal/jest.config.js
+++ web-portal/jest.config.js
@@ -13,7 +13,8 @@ module.exports = {
   'collectCoverage': true,
   'collectCoverageFrom': [
     '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
+    '<rootDir>/pages/**/*.vue',
+    '<rootDir>/services/**/*.js'
   ],
   testEnvironment: 'jsdom'
 }
