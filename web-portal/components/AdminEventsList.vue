9d7eb2e44ed12d107bdd2a10f4486d41e46f30be
Chris Wininger <chris.wininger@gmail.com>
2025-01-26 10:21:54 -0500

    Removes the remaining filters (#522)
    * Removes the remaining filters
    
    * move AdminEventsList Row
    
    * rename calendar_event to calendarEvent
    
    * remove last filter from code
    
    ---------
    
    Co-authored-by: chris <chris@pop-os.localdomain>

--- web-portal/components/AdminEventsList.vue
+++ web-portal/components/AdminEventsList.vue
@@ -10,47 +10,25 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="calendar_event in calendar_events" :key="calendar_event.id">
-        <td><img v-if="calendar_event.reviewed_by_org" :src="calendar_event.reviewed_by_org | ownerLogo" width="30" /></td>
-        <td>{{ calendar_event.title }}</td>
-        <td>
-          <div v-for="(date_time, index) in calendar_event.date_times.slice(0, 2)" :key="index">{{ date_time | dateFormat }}</div>
-          <small v-if="calendar_event.date_times.length > 2">(and {{ calendar_event.date_times.length - 2 }} more)</small>
-          <template v-if="calendar_event.category && calendar_event.category==='online-resource'">Online Resource</template>
-        </td>
-        <td>
-          <ii-form-button
-            style-type="light"
-            @click="onEditClicked(calendar_event.id)"
-            :test-id="'edit-issue-button-' + calendar_event.id"
-          >
-            Edit
-          </ii-form-button>
-        </td>
-        <td>
-          <input
-            type="checkbox"
-            :checked="isProblem(calendar_event)"
-            @change="isProblemUpdated(calendar_event, $event)"
-          >
-        </td>
-      </tr>
+      <ii-admin-events-list-row
+        v-for="calendarEvent in calendar_events"
+        :key="calendarEvent.id"
+        :calendar-event="calendarEvent"
+        @hasProblemsChanged="isProblemUpdatedChanged"
+      />
     </tbody>
   </table>
 </template>
 
 <script>
-  import momenttz from 'moment-timezone'
-
-  import PartnerService from '@/services/PartnerService'
   import { UPSERT_ADMIN_EVENT_METADATA } from '../store/event-admin-metadata'
   import getToken from '../helpers/getToken'
-  import FormButton from '@/components/FormButton.vue'
+  import AdminEventsListRow from '~/components/AdminEventsListRow.vue'
 
   export default {
     name: 'AdminEventsList',
     components: {
-      'ii-form-button': FormButton
+      'ii-admin-events-list-row': AdminEventsListRow
     },
     props: ['calendar_events'],
     data: function () {
@@ -58,42 +36,19 @@
         metadata: {}
       }
     },
-    filters: {
-      dateFormat: function (date) {
-        return [
-          momenttz(date.start_time).tz(date.timezone).format('dddd, MMMM Do h:mma'),
-          '-',
-          momenttz(date.end_time).tz(date.timezone).format('h:mma z')
-        ].join(' ')
-      },
-      ownerLogo: function (owner) {
-        return PartnerService.getLogoForReviewer(owner)
-      }
-    },
     methods: {
-      isProblem: function (calenderEvent) {
-        if (!calenderEvent.event_admin_meta_data) {
-          return false
-        } else {
-          return calenderEvent.event_admin_meta_data.is_problem
-        }
-      },
-
-      isProblemUpdated: function ({ id }, event) {
+      isProblemUpdatedChanged: function ({ id, checked }) {
         const idToken = getToken(this.$auth)
         if (!this.metadata[id]) {
           this.metadata[id] = {}
         }
 
-        this.metadata[id].isProblem = event.currentTarget.checked
+        this.metadata[id].isProblem = checked
 
         this.$store.dispatch(
           UPSERT_ADMIN_EVENT_METADATA,
-          { eventId: id, isProblem: event.currentTarget.checked, idToken }
+          { eventId: id, isProblem: checked, idToken }
         )
-      },
-      onEditClicked(eventId) {
-        this.$router.push({ name: 'admin-event-edit-id', params: { id: eventId } })
       }
     }
   }
