12e3d8c69f4d1a1ebe57da36e938a8241657d5b5
chris <chris@pop-os.localdomain>
2025-03-16 12:04:08 -0400

    use naming convention to determine http verb

--- web-portal-nuxt3/server/routes/api-internal/slack-analytics.post.js
+++ web-portal-nuxt3/server/routes/api-internal/slack-analytics.post.js
@@ -5,35 +5,32 @@ export const ENV = process.env.ENV || 'dev'
 
 export default defineEventHandler(async (event) => {
   const { slackWebhookAnalytics } = useRuntimeConfig(event);
-  logger.info('JavaScript HTTP trigger (analytics) function processed a request.')
+
+  logger.info('Analytics -- Processing submission suggestion feedback')
 
   if (!slackWebhookAnalytics) {
     logger.warn('Slack webhook (analytics) is not configured; will not be able to send messages')
   }
 
-  // deprecated now that we're also tracking brief description/"summary";
-  // prefer "suggestion-feedback"
-  if (isMethod(event, 'POST')) {
-    logger.info('Processing submission suggestion feedback')
-    const body = await readBody(event);
+  const body = await readBody(event);
 
-    if (body.suggested && body.submitted) {
-      try {
-        await PostToSlack('suggestion-feedback', body.suggested, body.submitted, body.eventId, slackWebhookAnalytics)
-      } catch (e) {
-        logger.error(e)
-        throw createError({
-          statusCode: 500,
-          statusMessage: 'could post suggestion-feedback analytics to slack channel'
-        })
-      }
-    } else {
+  if (body.suggested && body.submitted) {
+    try {
+      await PostToSlack('suggestion-feedback', body.suggested, body.submitted, body.eventId, slackWebhookAnalytics)
+    } catch (e) {
+      logger.error(e)
       throw createError({
-        statusCode: 400,
-        statusMessage: 'invalid request for suggestion-feedback analytics'
+        statusCode: 500,
+        statusMessage: 'could post suggestion-feedback analytics to slack channel'
       })
     }
+  } else {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'invalid request for suggestion-feedback analytics'
+    })
   }
+
 
   // more may be added here in the future
   return "";
