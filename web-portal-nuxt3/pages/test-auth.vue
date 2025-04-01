ca404cb5f9c36186c0c11d7a26b890dc86b2c13a
chris <chris@pop-os.localdomain>
2025-03-31 21:24:07 -0400

    testing

--- web-portal-nuxt3/pages/test-auth.vue
+++ web-portal-nuxt3/pages/test-auth.vue
@@ -1,71 +1,105 @@
 <template>
-  <div v-if="loggedIn">
-    <p>
-      Welcome User: {{user.login}}
-    </p>
-
-    <div>
-      <NuxtLink @click="clear">logout</NuxtLink>
+  <template>
+    <div class="container admin-page">
+      <h2>Unverified Events</h2>
+      <AdminEventsList :calendar_events="unverifiedEvents" class="unverified-events" />
+      <h2>Current Events</h2>
+      <Pagination
+        :items="verifiedEvents"
+        :max-number-of-page-shortcuts="maxNumberOfPageShortcuts"
+        class-name-page-list="ii-admin-page_current-events-pagination-list"
+      >
+        <AdminEventsList slot-scope="page" :calendar_events="page" class="current-events" />
+      </Pagination>
+      <h2>Resources</h2>
+      <Pagination
+        :items="resourceEvents"
+        :max-number-of-page-shortcuts="maxNumberOfPageShortcuts"
+        class-name-page-list="ii-admin-page_resources-pagination-list"
+      >
+        <admin-events-list slot-scope="page" :calendar_events="page" class="resources" />
+      </Pagination>
     </div>
-
-    <div class="test-class">
-      <ul>
-        <li v-for="event in events" :key="event.title">
-          {{ event.title }}
-        </li>
-      </ul>
-    </div>
-
-
-    <h2>Unverified Events</h2>
-    <admin-events-list :calendar_events="unverifiedEvents" class="unverified-events" />
-  </div>
-
-  <div v-else>
-    <p>
-      you are not logged in <NuxtLink to="/login">login</NuxtLink>
-    </p>
-  </div>
+  </template>
 </template>
 
 <script setup>
 import { useStore } from 'vuex'
-const { loggedIn, clear, user } = useUserSession()
-const config = useRuntimeConfig();
+const { user } = useUserSession();
+
+let isLessWindowLessThan900px = false;
+let _mediaQueryListener;
 
 const events = ref([])
-
 const store = useStore()
 
-await callOnce('LoadAdminPageDAta', async function () {
-  store.dispatch('admin/LoadUnverifiedEvents')
-  store.dispatch('admin/LoadCurrentEvents')
-  store.dispatch('admin/LoadResourceEvents')
-}, { mode: 'navigation' })
+// await callOnce('LoadAdminPageDAta', async function () {
+//   await store.dispatch('admin/LoadUnverifiedEvents')
+//   await store.dispatch('admin/LoadCurrentEvents')
+//   await store.dispatch('admin/LoadResourceEvents')
+// }, { mode: 'navigation' })
 
 const unverifiedEvents = computed(() => {
   return store.getters['admin/GetUnverifiedEvents']
 });
 
-const { data, error } = await useAsyncData('apiData', async () => {
-  if (loggedIn.value) {
-    const token = user.value?.token
-    const apiUrl = config.public.apiUrl;
+const verifiedEvents = computed(() => {
+  return store.getters['admin/GetVerifiedEvents']
+});
 
-    return await $fetch(`${apiUrl}/authenticated/events`, {
-      headers: {
-        'x-access-token': token
-      }
-    })
+const resourceEvents = computed(() => {
+  return store.getters['admin/GetResourceEvents']
+});
+
+const maxNumberOfPageShortcuts = computed(() => {
+  if (isLessWindowLessThan900px) {
+    return 5
   } else {
-    return Promise.resolve({ data: [], error: null })
+    return 25
   }
-})
+});
 
-const eventData = data?.value?.events || []
-events.value = eventData.sort((a, b) => a.id.localeCompare(b.id)).slice(0, 10)
+ onMounted(async () => {
+   console.log('!!! mounted')
+   store.dispatch('admin/LoadUnverifiedEvents')
+   store.dispatch('admin/LoadCurrentEvents')
+   store.dispatch('admin/LoadResourceEvents')
+
+
+   console.log('!!! mounted done')
+   if (window) {
+     _mediaQueryListener = window.matchMedia('(max-width: 900px)')
+    isLessWindowLessThan900px = _mediaQueryListener.matches
+
+     _mediaQueryListener.addEventListener('change', onMatchMediaChange)
+   }
+ });
+
+ onUnmounted(() => {
+   if (_mediaQueryListener) {
+     _mediaQueryListener.removeEventListener('change', onMatchMediaChange)
+     _mediaQueryListener = undefined
+   }
+ });
+
+ function onMatchMediaChange() {
+   isLessWindowLessThan900px = t_mediaQueryListener.matches
+ }
 </script>
 
 <style>
-  .test-class { color: white }
+  .calendar-events-table.unverified-events {
+    background: white;
+  }
+
+  .ii-pagination__list.ii-admin-page_current-events-pagination-list,
+  .ii-pagination__list.ii-admin-page_resources-pagination-list {
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 1rem;
+  }
+
+  .ii-pagination__list-wrapper {
+    display: flex;
+  }
 </style>
