57b247418fba530f9210c5c9a31b7a2b72f9db25
chris <chris@pop-os.localdomain>
2025-04-20 11:56:00 -0400

    calendar workish, stil need to fix wrapping components/v-model

--- web-portal-nuxt3/pages/test-auth.vue
+++ web-portal-nuxt3/pages/test-auth.vue
@@ -1,99 +1,35 @@
 <template>
   <div class="container admin-page">
-    <ClientOnly fallback-tag="span" fallback="Loading Admin Page...">
-      <h2>Unverified Events</h2>
-      <AdminEventsList :calendar_events="unverifiedEvents" />
-      <h2>Current Events</h2>
-      <Pagination
-        :items="verifiedEvents"
-        v-slot="page"
-        :max-number-of-page-shortcuts="maxNumberOfPageShortcuts"
-        class-name-page-list="ii-admin-page_current-events-pagination-list"
-      >
-        <AdminEventsList :calendar_events="page" class="current-events" />
-      </Pagination>
-      <h2>Resources</h2>
-      <Pagination
-        :items="resourceEvents"
-        v-slot="page"
-        :max-number-of-page-shortcuts="maxNumberOfPageShortcuts"
-        class-name-page-list="ii-admin-page_resources-pagination-list"
-      >
-        <admin-events-list :calendar_events="page" class="resources" />
-      </Pagination>
-    </ClientOnly>
+    <DatePicker :allow-past="allowPast" :date="picker" @change="dateChanged" />
+
+<!--    <date-time-picker v-model="dateTimes" :mode="'upload'" @change="onDateTimeVenueChanged" />-->
   </div>
 </template>
 
 <script setup>
-import { useStore } from 'vuex'
-const { user } = useUserSession();
 
-useHead({ title: 'Event Management - Infinite Industries' })
+const { picker, allowPast, dateTimes } = defineProps(['picker', 'allowPast', 'dateTimes' ])
 
-const isLessWindowLessThan900px = ref(false);
-const store = useStore()
+function onDateTimeVenueChanged() {
+  console.log('!!! onDateTimeVenueChanged');
+}
+// const props = defineProps({
+//   picker: {
+//     type: String, // Defines the type of the prop
+//     default: picker, // Sets a default value for the prop (optional)
+//   },
+//   allowPast: {
+//     type: Boolean,
+//     default: false
+//   }
+// });
 
-const unverifiedEvents = computed(() => {
-  return store.getters['admin/GetUnverifiedEvents']
-});
+const dataChanged = () => {
+  console.log('!!! data changed')
 
-const verifiedEvents = computed(() => {
-  return store.getters['admin/GetVerifiedEvents']
-});
-
-const resourceEvents = computed(() => {
-  return store.getters['admin/GetResourceEvents']
-});
-
-const maxNumberOfPageShortcuts = computed(() => {
-  return isLessWindowLessThan900px.value ? 5: 25;
-});
-
-let mediaQueryListener;
-onMounted(async () => {
-  if (process.client) {
-    console.log('!!! on client')
-    // create a change handler for screen size
-    mediaQueryListener = window.matchMedia('(max-width: 900px)')
-    isLessWindowLessThan900px.value = mediaQueryListener.matches
-    mediaQueryListener.addEventListener('change', onMatchMediaChange)
-
-    // load data
-    await store.dispatch('admin/LoadUnverifiedEvents')
-    await store.dispatch('admin/LoadCurrentEvents')
-    await store.dispatch('admin/LoadResourceEvents')
-  } else {
-    console.log('!!! on server')
-  }
-});
-
-onUnmounted(() => {
-  if (mediaQueryListener) {
-    mediaQueryListener.removeEventListener('change', onMatchMediaChange)
-    mediaQueryListener = undefined
-  }
-});
-
-
-function onMatchMediaChange() {
- isLessWindowLessThan900px.value = mediaQueryListener.matches
 }
 </script>
 
 <style>
-  .calendar-events-table.unverified-events {
-    background: white;
-  }
 
-  .ii-pagination__list.ii-admin-page_current-events-pagination-list,
-  .ii-pagination__list.ii-admin-page_resources-pagination-list {
-    margin-left: auto;
-    margin-right: auto;
-    margin-top: 1rem;
-  }
-
-  .ii-pagination__list-wrapper {
-    display: flex;
-  }
 </style>
