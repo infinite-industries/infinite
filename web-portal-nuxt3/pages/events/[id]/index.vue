08493970a0f39dae840cc7e90ba80003d2065529
Matthew Gidcomb <matthew.gidcomb@outlook.com>
2025-07-06 09:31:00 -0400

    Convert individual event page to pure composition API

--- web-portal-nuxt3/pages/events/[id]/index.vue
+++ web-portal-nuxt3/pages/events/[id]/index.vue
@@ -2,88 +2,80 @@
   <event-view :event="event" />
 </template>
 
-<script>
+<script setup>
   import EventView from '@/components/EventView.vue'
   import JsonLdService from '@/services/JsonLdService'
 
-  export default {
-    async setup () {
-      definePageMeta({
-        layout: 'no-mobile-cta',
-        validate: function ({ params }) {
-          return !!params.id
-        }
-      })
-
-      // fetch event and handle errors/missing
-      const { $apiService, $urlFor } = useNuxtApp()
-      const { params } = useRoute()
-      const { data, error } = await useAsyncData('event', () => $apiService.get('/events/' + params.id + '?embed=Venue'))
-      if (error.value) {
-        // TODO: current implementation doesn't neccessarily 404;
-        // TBD what to do
-        throw createError({ statusCode: error.value.statusCode, message: 'Not Found' })
-      }
-
-      // construct head metadata
-      const event = data.value.event
-      const eventId = event?.id ?? null
-      const title = event?.title ?? 'no event loaded'
-      // TODO: current view sanitizes brief_description; does vue-meta do that automatically?
-      // useHeadSafe's docs says it does
-      const description = event?.brief_description ?? ''
-      const url = $urlFor('/events/' + eventId)
-      const defaultImage = $urlFor('/images/default.jpg')
-
-      const eventImage = event?.image && event.image !== 'none' ? event.image : defaultImage
-      const socialImage = (event && event.social_image && event.social_image !== 'none'
-        ? event.social_image
-        : eventImage)
-
-      const jsonLdData = JsonLdService.forEvent(event)
-
-      useHead({
-        title, // TODO: why doesn't this work with useHeadSafe?
-      })
-      useHeadSafe({
-        // title,
-        meta: [
-          { name: 'title', content: title },
-          { name: 'description', content: description },
-
-          { property: 'og:title', content: title },
-          { property: 'og:description', content: description },
-          { property: 'og:url', content: url },
-          { property: 'og:type', content: 'article' },
-          { property: 'og:image', content: socialImage },
-          { property: 'og:image:alt', content: 'Check out ' + title },
-
-          { name: 'twitter:card', content: 'summary_large_image' },
-          { name: 'twitter:title', content: title },
-          { name: 'twitter:description', content: description },
-          { name: 'twitter:image:src', content: socialImage },
-
-          // TODO: is this setting actually what we want?
-          { hid: 'robots', name: 'robots', content: 'index, noarchive, nocache' }
-        ],
-        link: [
-          { rel: 'canonical', href: $urlFor('/events/' + event.id)}
-        ],
-        ...(jsonLdData
-          ? {
-            script: [
-              { type: 'application/ld+json', textContent: jsonLdData }
-            ]
-          }
-          : null)
-      })
-
-      return {
-        event
-      }
-    },
-    components: {
-      'event-view': EventView
+  definePageMeta({
+    layout: 'no-mobile-cta',
+    validate: function ({ params }) {
+      return !!params.id
     }
+  })
+
+  const event = ref(null)
+
+  // fetch event and handle errors/missing
+  const { $apiService, $urlFor } = useNuxtApp()
+  const { params } = useRoute()
+  const { data, error } = await useAsyncData('event', () => $apiService.get('/events/' + params.id + '?embed=Venue'))
+  if (error.value) {
+    // TODO: current implementation doesn't neccessarily 404;
+    // TBD what to do
+    throw createError({ statusCode: error.value.statusCode, message: 'Not Found' })
   }
+
+  // construct head metadata
+  event.value = data.value.event
+  const eventId = event.value?.id ?? null
+  const title = event.value?.title ?? 'no event loaded'
+  // TODO: current view sanitizes brief_description; does vue-meta do that automatically?
+  // useHeadSafe's docs says it does
+  const description = event.value?.brief_description ?? ''
+  const url = $urlFor('/events/' + eventId)
+  const defaultImage = $urlFor('/images/default.jpg')
+
+  const eventImage = event.value?.image && event.image !== 'none' ? event.value.image : defaultImage
+  const socialImage = (event.value && event.value.social_image && event.value.social_image !== 'none'
+    ? event.value.social_image
+    : eventImage)
+
+  const jsonLdData = JsonLdService.forEvent(event.value)
+
+  useHead({
+    title, // TODO: why doesn't this work with useHeadSafe?
+    // TODO: or link?
+    link: [
+      { rel: 'canonical', href: $urlFor('/events/' + eventId) }
+    ],
+    ...(jsonLdData
+      ? {
+        script: [
+          { type: 'application/ld+json', innerHTML: jsonLdData }
+        ]
+      }
+      : null)
+  })
+  useHeadSafe({
+    // title,
+    meta: [
+      { name: 'title', content: title },
+      { name: 'description', content: description },
+
+      { property: 'og:title', content: title },
+      { property: 'og:description', content: description },
+      { property: 'og:url', content: url },
+      { property: 'og:type', content: 'article' },
+      { property: 'og:image', content: socialImage },
+      { property: 'og:image:alt', content: 'Check out ' + title },
+
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:title', content: title },
+      { name: 'twitter:description', content: description },
+      { name: 'twitter:image:src', content: socialImage },
+
+      // TODO: is this setting actually what we want?
+      { name: 'robots', content: 'index, noarchive, nocache' }
+    ]
+  })
 </script>
