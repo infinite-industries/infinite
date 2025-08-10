2c9d68aa63506ae540cd7d683e6263aab4308ca3
Matthew Gidcomb <matthew.gidcomb@outlook.com>
2025-08-09 21:05:44 -0400

    Troubleshooting

--- e2e-tests/plugins/index.js
+++ e2e-tests/plugins/index.js
@@ -14,4 +14,7 @@
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  on('uncaught:exception', (...args) => {
+    console.error(`\n\neating uncaught exception: ${args.join(', ')}`)
+  })
 }
