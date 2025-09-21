6dba5af1959b93fce63358ec1012ff1f93fef6a0
chris <chris.wininger@gmail.com>
2025-09-21 13:32:52 -0400

    quick fix - delete duplicate code

--- api-server/src/events/events.controller.ts
+++ api-server/src/events/events.controller.ts
@@ -40,11 +40,11 @@ import {
   PaginationDto,
 } from './dto/pagination-dto';
 import { isNullOrUndefined } from '../utils';
-import { validateAndExtractOptionalDateTimeFilters } from './utils/validateAndExtractOptionalDatetimeFilters';
 import { VenueModel } from 'src/venues/models/venue.model';
 import { DatetimeVenueModel } from './models/datetime-venue.model';
 import { EventAdminMetadataModel } from './models/event-admin-metadata.model';
 import isAdminUser from 'src/authentication/is-admin-user';
+import { validateAndExtractOptionalDateTimeFilters } from './utils/validateAndExtractOptionalDatetimeFilters';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
@@ -254,45 +254,5 @@ export class EventsController {
     } catch (exSlack) {
       this.logger.error(`error notifying slack of new event: ${exSlack}`);
     }
-  }
-
-  private validateAndExtractOptionalDateTimeFilters(
-    dateRange?: string,
-  ): [Date | undefined, Date | undefined] {
-    if (!dateRange) {
-      return [undefined, undefined];
-    }
-
-    const dates = dateRange.split('/');
-    if (dates.length !== 2) {
-      throw new BadRequestException(
-        'Invalid dateRange format. Expected format: startDate/endDate (e.g., 2024-01-01T00:00:00.000Z/2024-12-31T23:59:59.999Z)',
-      );
-    }
-
-    const [startDateStr, endDateStr] = dates;
-
-    if (!startDateStr || !endDateStr) {
-      throw new BadRequestException(
-        'Both startDate and endDate must be provided in dateRange parameter',
-      );
-    }
-
-    const startDate = new Date(startDateStr);
-    const endDate = new Date(endDateStr);
-
-    if (isNaN(startDate.getTime()) || isNaN(endDate.getTime())) {
-      throw new BadRequestException(
-        'Invalid date format in dateRange. Ensure dates are in ISO 8601 format.',
-      );
-    }
-
-    if (startDate >= endDate) {
-      throw new BadRequestException(
-        'startDate must be before endDate in dateRange parameter',
-      );
-    }
-
-    return [startDate, endDate];
   }
 }
