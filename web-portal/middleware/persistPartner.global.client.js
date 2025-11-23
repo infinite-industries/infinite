5538f4eb846d33c5ddf228e41d054e38a0689e76
chris <chris.wininger@gmail.com>
2025-11-23 10:27:18 -0500

    reverting middleware changes

--- web-portal/middleware/persistPartner.global.client.js
+++ web-portal/middleware/persistPartner.global.client.js
@@ -1,18 +1,14 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  const partnerQuery = from.query.partner
-
-  console.log('!!! middleware running - partnerQuery:', partnerQuery, 'to.path:', to.path)
-  if (partnerQuery && !to.query.partner) {
-    // Preserve the 'partner' query parameter when it exists
-    console.log('!!! preserving partner param:', partnerQuery)
-    return navigateTo({
-      path: to.path,
-      query: { ...to.query, partner: partnerQuery },
-      hash: to.hash,
-      params: to.params,
-    })
-  }
-
-  return;
-})
-
+    const partnerQuery = from.query.partner
+  
+    if (partnerQuery && !to.query.partner) {
+      // Preserve the 'partner' query parameter when it exists
+      return navigateTo({
+        path: to.path,
+        query: { ...to.query, partner: partnerQuery },
+        hash: to.hash,
+        params: to.params,
+      })
+    }
+  })
+  
